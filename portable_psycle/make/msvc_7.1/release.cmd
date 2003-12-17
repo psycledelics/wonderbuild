bb5f639ccd59b5b360078f98cc1340d6c5c50ca6
(no author) <(no author)>
2003-12-17 06:17:44 +0000

    This commit was manufactured by cvs2svn to create branch 'experimental'.
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/experimental@495 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/release.cmd
+++
@@ -1,7 +0,0 @@
-call "%VS71ComnTools%\VSVars32"
-DevEnv solution.sln /clean release /out clean.log
-rem del/s/q clean.log
-DevEnv solution.sln /build release /out release.log
-call upx
-rar a -s -m5 -md4096 softsynth__release__bin.rar release\bin ..\..\doc\history.text 1>> release.log 2>&1
-pause
