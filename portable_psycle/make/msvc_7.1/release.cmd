c96f3ff5f3b67ac5acb165339a922e664bbc3f3f
Johan Boule <johan.boule@retropaganda.info>
2004-04-29 04:23:36 +0000

    fixed scripts
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@532 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/release.cmd
+++ portable_psycle/make/msvc_7.1/release.cmd
@@ -1,11 +1,26 @@
 call "%VS71ComnTools%\VSVars32"
-DevEnv solution.sln /clean release /out clean.log
-rem del/s/q clean.log
-DevEnv solution.sln /build release /out release.log
-call upx
+DevEnv solution.sln /clean release /out clean.log || ( echo clean failed, aborting. & goto :pause )
+rem del/q clean.log
+DevEnv solution.sln /build release /out release.log || ( echo build failed, aborting. & goto :pause )
+
+call upx || ( echo upx failed, aborting. & goto :pause )
+
+rmdir/s/q .\psycle\
+mkdir .\psycle\
+xcopy .\release.bin.upx.nrv2d\psycle.exe .\psycle\ || ( echo copy failed, aborting. & goto :pause )
+rem mkdir .\psycle\plugins\
+rem xcopy .\release.bin\psycle__plugins\*.dll .\psycle\plugins\ || ( echo copy failed, aborting. & goto :pause )
+mkdir .\psycle\doc\
+xcopy/s ..\..\doc\for-end-users\* .\psycle\doc\ || ( echo copy failed, aborting. & goto :pause )
+for /r .\psycle\doc\ %%i in (*.txt) do unix2dos "%%i"
+
 del/q .\psycle.bin.rar
-rar a -s -m5 -md4096 -ep1 -r0 .\psycle.bin.rar .\release.bin.upx.nrv2b\*.exe .\release.bin.upx.nrv2b\*.dll ..\..\doc\for-end-users 1>> .\release.log 2>&1
-scp ./psycle.bin.rar johan-boule@shell.sourceforge.net:~/psycle/htdocs/ || ( echo scp failed, continuing with next hosts... )
-rem other hosts?
+rar a -s -m5 -md4096 -ep1 -r0 .\psycle.bin.rar .\psycle 1>> .\release.log 2>&1 || ( echo rar failed, aborting. & goto :pause )
+
+scp ./psycle.bin.rar johan-boule@shell.sourceforge.net:psycle/htdocs/ || ( echo scp failed, aborting. && goto :pause )
+ssh johan-boule@shell.sourceforge.net psycle/htdocs.update.bash || ( echo ssh failed, aborting. && goto :pause )
+
+:pause
 pause
-rem del/q .\psycle.bin.rar
+rmdir/s/q .\psycle\
+del/q .\psycle.bin.rar
--- portable_psycle/make/msvc_7.1/upx.cmd
+++ portable_psycle/make/msvc_7.1/upx.cmd
@@ -1,14 +1,14 @@
 for %%t in (nrv2b nrv2d) do (
-	del/s/q/ar .\release.bin.upx.%%t\
+	rmdir/s/q .\release.bin.upx.%%t\
 	mkdir .\release.bin.upx.%%t\
 	for /r .\release\bin\ %%i in (*.exe *.dll) do (
-		call :upx %%t "%%i" || goto :pause
+		call :upx %%t "%%i" || ( echo upx failed, aborting. && goto :pause )
 	)
 )
 goto :eof
 
 :upx
-upx.exe --overlay=strip --force --strip-relocs=1 --compress-icons=1 --best --crp-ms=999999 --%1 --no-backup .\release.bin.upx.%1\%~n2%~x2 %2
+upx.exe --overlay=strip --force --strip-relocs=1 --compress-icons=1 --best --crp-ms=999999 --%1 --no-backup -o .\release.bin.upx.%1\%~n2%~x2 %2
 goto :eof
 
 :pause
