7a7338fbe9d0b3e6a3e5382581670b43793d321c
Johan Boule <johan.boule@retropaganda.info>
2004-04-30 17:53:33 +0000

    added a config for intel pentium 4
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@549 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/release.cmd
+++ portable_psycle/make/msvc_7.1/release.cmd
@@ -3,68 +3,87 @@ rem clean and build
 rem ===============
 
 call "%VS71ComnTools%\VSVars32"
+
+rem ##############################
+rem ######### amd k7 and intel p3
 DevEnv solution.sln /clean release /out clean.log || ( echo clean failed, aborting. & goto :pause )
-DevEnv solution.sln /clean release_intel_pentium_4 /out clean.log || ( echo clean failed, aborting. & goto :pause )
 del/q clean.log
 DevEnv solution.sln /build release /out release.log || ( echo build failed, aborting. & goto :pause )
+
+rem ####################
+rem ########## intel p4
+DevEnv solution.sln /clean release_intel_pentium_4 /out clean.log || ( echo clean failed, aborting. & goto :pause )
+del/q clean.log
 DevEnv solution.sln /build release_intel_pentium_4 /out release.log || ( echo build failed, aborting. & goto :pause )
-
-rem ========
-rem compress
-rem ========
-
-call upx || ( echo upx failed, aborting. & goto :pause )
 
 rem ====================================
 rem make the directory to be distributed
 rem ====================================
 
-rmdir/s/q .\psycle\
-mkdir .\psycle\
+set timestamp=%date%.%time%
+set timestamp=%timestamp: =.%
+set timestamp=%timestamp:/=.%
+set timestamp=%timestamp:\=.%
+set timestamp=%timestamp::=.%
+set timestamp=%timestamp:-=.%
+
+set distribution=.\psycle-%timestamp%\
+
+rmdir/s/q "%distribution%"
+mkdir "%distribution%"
 
 rem ------------------------------
 rem copy executables and libraries
 rem ------------------------------
 
-xcopy/f .\release.bin.upx.nrv2d\psycle.exe .\psycle\ || ( echo copy failed, aborting. & goto :pause )
-xcopy/f .\release_intel_pentium_4.bin.upx.nrv2d\psycle.exe .\psycle\psycle-intel-pentium-4.exe || ( echo copy failed, aborting. & goto :pause )
+rem ###############################
+rem ########## amd k7 and intel p3
+xcopy/f/i ".\release\bin\psycle.exe" "%distribution%" || ( echo copy failed, aborting. & goto :pause )
+rename "%distribution%\psycle.exe" "psycle-%timestamp%.exe" || ( echo rename failed, aborting. & goto :pause )
+call :upx "%distribution%\psycle-%timestamp%.exe"
 
-rem mkdir .\psycle\plugins\
-rem xcopy/f .\release.bin\psycle__plugins\*.dll .\psycle\plugins\ || ( echo copy failed, aborting. & goto :pause )
+rem ####################
+rem ########## intel p4
+xcopy/f/i ".\release_intel_pentium_4\bin\psycle.exe" "%distribution%" || ( echo copy failed, aborting. & goto :pause )
+rename "%distribution%\psycle.exe" "psycle-%timestamp%-intel-pentium-4.exe" || ( echo rename failed, aborting. & goto :pause )
+call :upx "%distribution%\psycle-%timestamp%-intel-pentium-4.exe"
+
+rem mkdir "%distribution%\plugins\"
+rem xcopy/f .\release.bin\psycle__plugins\*.dll "%distribution%\plugins\" || ( echo copy failed, aborting. & goto :pause )
 
 rem ------------------------------------------
 rem copy microsoft c/c++/mfc runtime libraries
 rem ------------------------------------------
 
-xcopy/f "%SYSTEMROOT%\system32\msvcr71.dll" .\psycle\ || ( echo copy failed, aborting. & goto :pause )
-xcopy/f "%SYSTEMROOT%\system32\msvcp71.dll" .\psycle\ || ( echo copy failed, aborting. & goto :pause )
-xcopy/f "%SYSTEMROOT%\system32\mfc71.dll" .\psycle\ || ( echo copy failed, aborting. & goto :pause )
+xcopy/f "%SYSTEMROOT%\system32\msvcr71.dll" "%distribution%" || ( echo copy failed, aborting. & goto :pause )
+xcopy/f "%SYSTEMROOT%\system32\msvcp71.dll" "%distribution%" || ( echo copy failed, aborting. & goto :pause )
+xcopy/f "%SYSTEMROOT%\system32\mfc71.dll" "%distribution%" || ( echo copy failed, aborting. & goto :pause )
 
 rem ---------------------------
 rem copy end-user documentation
 rem ---------------------------
 
-mkdir .\psycle\doc\
-xcopy/f/s ..\..\doc\for-end-users\* .\psycle\doc\ || ( echo copy failed, aborting. & goto :pause )
+mkdir "%distribution%\doc\"
+xcopy/f/s ..\..\doc\for-end-users\* "%distribution%\doc\" || ( echo copy failed, aborting. & goto :pause )
 
 rem ----------------
 rem remove cvs files
 rem ----------------
 
-for /r .\psycle\ %%i in (CVS) do rmdir/s/q "%%i"
-for /r .\psycle\ %%i in (.cvsignore) do del/q "%%i"
+for /r "%distribution%" %%i in (CVS) do rmdir/s/q "%%i"
+for /r "%distribution%" %%i in (.cvsignore) do del/q "%%i"
 
 rem ----------------------------------------------------------------------------------
 rem convert text files to microsoft end-of-lines (unix2dos is distributed with cygwin)
 rem ----------------------------------------------------------------------------------
-for /r .\psycle\ %%i in (*.txt *.text) do unix2dos "%%i"
+for /r "%distribution%" %%i in (*.txt *.text) do unix2dos "%%i"
 
 rem ================
 rem make the archive
 rem ================
 
 del/q .\psycle.bin.rar
-rar a -s -m5 -md4096 -ep1 -r0 .\psycle.bin.rar .\psycle 1>> .\release.log 2>&1 || ( echo rar failed, aborting. & goto :pause )
+rar a -s -m5 -md4096 -ep1 -r0 .\psycle.bin.rar "%distribution%" 1>> .\release.log 2>&1 || ( echo rar failed, aborting. & goto :pause )
 
 rem =====================================================================================
 rem upload the archive and update the site (scp and ssh are cygwin commands, use / not \)
@@ -81,5 +100,14 @@ rem ================================
 
 :pause
 pause
-rmdir/s/q .\psycle\
+rmdir/s/q "%distribution%"
 del/q .\psycle.bin.rar
+goto :eof
+
+rem ===============
+rem upx sub routine
+rem ===============
+
+:upx
+upx.exe --overlay=strip --force --strip-relocs=1 --compress-icons=1 --best --crp-ms=999999 --nrv2d --no-backup -o %~p1%~n1.upx.%~x1 %1
+goto :eof
