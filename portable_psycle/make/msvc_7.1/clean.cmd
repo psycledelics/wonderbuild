cfc15337cd154181b0e16628bfc9d81c2147bf2a
Johan Boule <johan.boule@retropaganda.info>
2004-05-05 02:07:16 +0000

    1.7.6.2 added compatibility for mswin98 (removed use of unicows.dll)
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@561 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/clean.cmd
+++ portable_psycle/make/msvc_7.1/clean.cmd
@@ -13,16 +13,7 @@ del/s/q "*.sbr"
 del/s/q "*.bsc"
 del/s/q "*.lib"
 del/s/q "*.exp"
-
-rename .\release\bin\unicows.dll unicows.dll.preserve
-rename .\release_intel_pentium_4\bin\unicows.dll unicows.dll.preserve
-rename .\debug\bin\unicows.dll unicows.dll.preserve
 del/s/q "*.dll"
-del/s/q/ar "*.dll"
-rename .\release\bin\unicows.dll.preserve unicows.dll
-rename .\release_intel_pentium_4\bin\unicows.dll.preserve unicows.dll
-rename .\debug\bin\unicows.dll.preserve unicows.dll
-
 del/s/q "*.exe"
 del/s/q "*.opt"
 del/s/q "*.ncb"
