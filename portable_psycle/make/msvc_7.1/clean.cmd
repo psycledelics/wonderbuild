ace74cef3481832090e902d6fa6645011ea2fb5c
Johan Boule <johan.boule@retropaganda.info>
2004-06-07 03:10:41 +0000

    added boost libraries
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@771 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/clean.cmd
+++ portable_psycle/make/msvc_7.1/clean.cmd
@@ -14,7 +14,25 @@ del/s/q "*.sbr"
 del/s/q "*.bsc"
 del/s/q "*.lib"
 del/s/q "*.exp"
+
+rename .\release.g7\lib\boost_thread.lib boost_thread.lib.preserve
+rename .\release.g6\lib\boost_thread.lib boost_thread.lib.preserve
+rename .\debug\lib\boost_threadd.lib boost_threadd.lib.preserve
+del/s/q "*.lib"
+del/s/q/ar "*.lib"
+rename .\release.g7\lib\boost_thread.lib.preserve boost_thread.lib
+rename .\release.g6\lib\boost_thread.lib.preserve boost_thread.lib
+rename .\debug\lib\boost_threadd.lib.preserve boost_threadd.lib
+
+rename .\release.g7\bin\boost_thread.dll boost_thread.dll.preserve
+rename .\release.g6\bin\boost_thread.dll boost_thread.dll.preserve
+rename .\debug\bin\boost_threadd.dll boost_threadd.dll.preserve
 del/s/q "*.dll"
+del/s/q/ar "*.dll"
+rename .\release.g7\bin\boost_thread.dll.preserve boost_thread.dll
+rename .\release.g6\bin\boost_thread.dll.preserve boost_thread.dll
+rename .\debug\bin\boost_threadd.dll.preserve boost_threadd.dll
+
 del/s/q "*.exe"
 del/s/q "*.opt"
 del/s/q "*.ncb"
