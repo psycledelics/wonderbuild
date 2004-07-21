81af7e7b1e8f0e7008c9abe720607356273a082e
Johan Boule <johan.boule@retropaganda.info>
2004-07-21 23:31:40 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@782 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/clean.cmd
+++ portable_psycle/make/msvc_7.1/clean.cmd
@@ -2,49 +2,49 @@ rem ===================================
 rem cleans all compiler-generated files
 rem ===================================
 
-del/s/q "*.pch"
-del/s/q "*.obj"
-del/s/q "*.res"
-del/s/q "*.aps"
-del/s/q "..\..\src\*.aps"
-del/s/q "*.idb"
-del/s/q "*.pdb"
-del/s/q "*.ilk"
-del/s/q "*.sbr"
-del/s/q "*.bsc"
-del/s/q "*.exp"
+del/s/q "*.pch" || goto :failed
+del/s/q "*.obj" || goto :failed
+del/s/q "*.res" || goto :failed
+del/s/q "*.aps" || goto :failed
+del/s/q "..\..\src\*.aps" || goto :failed
+del/s/q "*.idb" || goto :failed
+del/s/q "*.ilk" || goto :failed
+del/s/q "*.sbr" || goto :failed
+del/s/q "*.bsc" || goto :failed
+del/s/q "*.exp" || goto :failed
 
-rename .\release.g7\lib\boost_thread.lib boost_thread.lib.preserve
-rename .\release.g6\lib\boost_thread.lib boost_thread.lib.preserve
-rename .\debug\lib\boost_threadd.lib boost_threadd.lib.preserve
-del/s/q "*.lib"
-del/s/q/ar "*.lib"
-rename .\release.g7\lib\boost_thread.lib.preserve boost_thread.lib
-rename .\release.g6\lib\boost_thread.lib.preserve boost_thread.lib
-rename .\debug\lib\boost_threadd.lib.preserve boost_threadd.lib
+rename .\release.g7\lib\boost_thread.lib boost_thread.lib.preserve || goto :failed
+rename .\release.g6\lib\boost_thread.lib boost_thread.lib.preserve || goto :failed
+rename .\debug\lib\boost_threadd.lib boost_threadd.lib.preserve || goto :failed
+del/s/q "*.lib" || goto :failed
+del/s/q/ar "*.lib" || goto :failed
+rename .\release.g7\lib\boost_thread.lib.preserve boost_thread.lib || goto :failed
+rename .\release.g6\lib\boost_thread.lib.preserve boost_thread.lib || goto :failed
+rename .\debug\lib\boost_threadd.lib.preserve boost_threadd.lib || goto :failed
 
-rename .\release.g7\bin\boost_thread.dll boost_thread.dll.preserve
-rename .\release.g6\bin\boost_thread.dll boost_thread.dll.preserve
-rename .\debug\bin\boost_threadd.dll boost_threadd.dll.preserve
-del/s/q "*.dll"
-del/s/q/ar "*.dll"
-rename .\release.g7\bin\boost_thread.dll.preserve boost_thread.dll
-rename .\release.g6\bin\boost_thread.dll.preserve boost_thread.dll
-rename .\debug\bin\boost_threadd.dll.preserve boost_threadd.dll
+rename .\release.g7\bin\boost_thread.dll boost_thread.dll.preserve || goto :failed
+rename .\release.g6\bin\boost_thread.dll boost_thread.dll.preserve || goto :failed
+rename .\debug\bin\boost_threadd.dll boost_threadd.dll.preserve || goto :failed
+del/s/q "*.dll" || goto :failed
+del/s/q/ar "*.dll" || goto :failed
+rename .\release.g7\bin\boost_thread.dll.preserve boost_thread.dll || goto :failed
+rename .\release.g6\bin\boost_thread.dll.preserve boost_thread.dll || goto :failed
+rename .\debug\bin\boost_threadd.dll.preserve boost_threadd.dll || goto :failed
 
-del/s/q "*.exe"
-del/s/q "*.opt"
-del/s/q "*.ncb"
-del/s/q/ah "*.suo"
-del/s/q "*.log"
-del/s/q "BuildLog.htm*"
+del/s/q "*.pdb" || goto :failed
+del/s/q "*.exe" || goto :failed
+del/s/q "*.opt" || goto :failed
+del/s/q "*.ncb" || goto :failed
+del/s/q/ah "*.suo" || goto :failed
+del/s/q "*.log" || goto :failed
+del/s/q "BuildLog.htm*" || goto :failed
 
 rem =================================
 rem cleans all psycle-generated files
 rem =================================
 
-del/s/q plugin-scan*
-del/s/q output.log.txt
+del/s/q plugin-scan* || goto :failed
+del/s/q output.log.txt || goto :failed
 
 rem ===========================================
 rem safely recursively removes empty directories
@@ -52,8 +52,14 @@ rem ===========================================
 
 call :prune .
 goto :eof
+
 :prune
 for /d %%i in (%1\*) do (
 	call :prune "%%i"
 	rmdir "%%i"
 )
+goto :eof
+
+:failed
+echo %~n0 failed.
+pause
