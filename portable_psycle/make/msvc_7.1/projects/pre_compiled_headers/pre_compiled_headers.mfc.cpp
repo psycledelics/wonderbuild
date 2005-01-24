dcbd01b96b5eef15fefc8a0ba7b8c6f9f6309ea1
Johan Boule <johan.boule@retropaganda.info>
2005-01-24 14:53:55 +0000

    This commit is going to be annoying for other people. I have moved the header files whose implementation was in the src dir from the include dir to the src dir, along with their source, implementation files. You can retrieve the tagged revision set release_1_7_25 if needed to go back just before this change. The include dir still contains header files for libraries that we don't compile ourself, like the boost lib, for which the cvs just distribute the compiled libraries, and the vst headers (there's no vst compiled library tho, they are just dumb declarations). Other third-party code, namely, asio, winamp-2, foobar2000, and stk have .cpp files which we compile ourself, hence they are in the src dir. Of course, all of psycle's own code in the the src dir, nothing of it is in the include dir anymore.
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@965 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/projects/pre_compiled_headers/pre_compiled_headers.mfc.cpp
+++
@@ -1,3 +0,0 @@
-#include <stdafx.h>
-#pragma hdrstop // borland and msvc: instructs the compiler to stop pre-compiling headers
-void attribute(dllexport) pre_compiled_headers() {} // a dummy function needed to force msvc to generate something
