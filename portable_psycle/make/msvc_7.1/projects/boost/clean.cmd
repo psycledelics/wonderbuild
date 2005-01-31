a8f2135f5eb09288a90153f6528c4b0ef4f80058
Johan Boule <johan.boule@retropaganda.info>
2005-01-31 10:54:50 +0000

    DOS is nut
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1032 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/projects/boost/clean.cmd
+++ portable_psycle/make/msvc_7.1/projects/boost/clean.cmd
@@ -1,6 +1,25 @@
 pushd ..\..\..\..\include\ && (
 	if exist boost (
 		rmdir/s/q boost
+		rem [bohan] i don't know why sometimes it fails
+		if exist boost (
+			rmdir/s/q boost
+			if exist boost (
+				rmdir/s/q boost
+				if exist boost (
+					rmdir/s/q boost
+					if exist boost (
+						rmdir/s/q boost
+						if exist boost (
+							rmdir/s/q boost
+							if exist boost (
+								rmdir/s/q boost
+							)
+						)
+					)
+				)
+			)
+		)
 	)
 	popd
 )
