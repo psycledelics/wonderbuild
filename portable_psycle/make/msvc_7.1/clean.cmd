c434db813ac6bf84c19eccee2365bf2ed92d1a4d
Johan Boule <johan.boule@retropaganda.info>
2005-03-04 04:09:33 +0000

    added .cvsignore files
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1069 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/clean.cmd
+++ portable_psycle/make/msvc_7.1/clean.cmd
@@ -1,56 +1,14 @@
-rem ===================================
-rem cleans all compiler-generated files
-rem ===================================
+rem ===========================================
+rem cleans all IDE and compiler generated files
+rem ===========================================
 
-del/s/q "*.pch" || goto :failed
-del/s/q "*.obj" || goto :failed
-del/s/q "*.res" || goto :failed
-del/s/q "*.aps" || goto :failed
-rem del/s/q "..\..\src\*.aps" || goto :failed
-del/s/q "*.idb" || goto :failed
-del/s/q "*.ilk" || goto :failed
-del/s/q "*.sbr" || goto :failed
-del/s/q "*.bsc" || goto :failed
-del/s/q "*.exp" || goto :failed
+rmdir/s/q output || goto failed
 
-rename .\release.g7\lib\boost_thread.lib boost_thread.lib.preserve || goto :failed
-rename .\release.g6\lib\boost_thread.lib boost_thread.lib.preserve || goto :failed
-rename .\debug\lib\boost_threadd.lib boost_threadd.lib.preserve || goto :failed
-del/s/q "*.lib" || goto :failed
-del/s/q/ar "*.lib" || goto :failed
-rename .\release.g7\lib\boost_thread.lib.preserve boost_thread.lib || goto :failed
-rename .\release.g6\lib\boost_thread.lib.preserve boost_thread.lib || goto :failed
-rename .\debug\lib\boost_threadd.lib.preserve boost_threadd.lib || goto :failed
+call projects\boost\clean.cmd
+call projects\doxygen\clean.cmd
 
-rename .\release.g7\bin\boost_thread.dll boost_thread.dll.preserve || goto :failed
-rename .\release.g6\bin\boost_thread.dll boost_thread.dll.preserve || goto :failed
-rename .\debug\bin\boost_threadd.dll boost_threadd.dll.preserve || goto :failed
-del/s/q "*.dll" || goto :failed
-del/s/q/ar "*.dll" || goto :failed
-rename .\release.g7\bin\boost_thread.dll.preserve boost_thread.dll || goto :failed
-rename .\release.g6\bin\boost_thread.dll.preserve boost_thread.dll || goto :failed
-rename .\debug\bin\boost_threadd.dll.preserve boost_threadd.dll || goto :failed
-
-del/s/q "*.exe" || goto :failed
-del/s/q "*.pdb" || goto :failed
-del/s/q "*.opt" || goto :failed
 del/s/q "*.ncb" || goto :failed
 del/s/q/ah "*.suo" || goto :failed
-del/s/q "*.log" || goto :failed
-del/s/q "BuildLog.htm*" || goto :failed
-
-rem ============
-rem cleans boost
-rem ============
-
-rmdir/s/q ..\..\include\boost
-
-rem =================================
-rem cleans all psycle-generated files
-rem =================================
-
-del/s/q "*.log.txt" || goto :failed
-del/s/q "*.cache" || goto :failed
 
 rem ============================================
 rem safely recursively removes empty directories
