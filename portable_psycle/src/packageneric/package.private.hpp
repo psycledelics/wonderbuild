4515c693b1f4eacd85bb7490af07302869ae26f6
Johan Boule <johan.boule@retropaganda.info>
2006-02-07 00:22:07 +0000

    *** empty log message ***
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1422 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ portable_psycle/src/packageneric/package.private.hpp
@@ -0,0 +1,30 @@
+///\file
+///\brief description of a fake source package (there's no notion of source package in psycle's cvs).
+#pragma once
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///\name source package meta information
+///\{
+	/// name of the source package
+	#define PACKAGENERIC__PACKAGE__NAME "psycle-cvs"
+	/// description of the source package
+	#define PACKAGENERIC__PACKAGE__DESCRIPTION "modular music studio"
+	/// version of the source package
+	#define PACKAGENERIC__PACKAGE__VERSION PACKAGENERIC__PACKAGE__VERSION__MAJOR "." PACKAGENERIC__PACKAGE__VERSION__MINOR "." PACKAGENERIC__PACKAGE__VERSION__PATCH
+	/// major version number of the source package
+	#define PACKAGENERIC__PACKAGE__VERSION__MAJOR 1
+	/// minor version number of the source package
+	#define PACKAGENERIC__PACKAGE__VERSION__MINOR 9
+	/// patch version number of the source package
+	#define PACKAGENERIC__PACKAGE__VERSION__PATCH 4
+	/// distribution archive (e.g., unstable, testing, stable)
+	#define PACKAGENERIC__PACKAGE__ARCHIVE "unstable"
+	/// origin
+	#define PACKAGENERIC__PACKAGE__ORIGIN "the psycle project http://psycle.sourceforge.net"
+	/// copyright
+	#define PACKAGENERIC__PACKAGE__COPYRIGHT "Copyright (C) 1999-2006 the psycledelics community http://psycle.pastnotecut.org"
+///\}
