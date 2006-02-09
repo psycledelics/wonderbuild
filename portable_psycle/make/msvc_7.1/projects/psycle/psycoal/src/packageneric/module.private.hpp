89323f8d20c7d1e01157b6c35c0ea09948d1caf2
jaz001 <jaz001>
2006-02-09 23:06:14 +0000

    Added some bits of psycoal
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1428 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ portable_psycle/make/msvc_7.1/projects/psycle/psycoal/src/packageneric/module.private.hpp
@@ -0,0 +1,9 @@
+///\file
+///\brief packageneric configuration for module psycoal
+#pragma once
+#include <packageneric/configuration.private.hpp>
+
+#define PACKAGENERIC__MODULE__NAME "psycoal"
+#define PACKAGENERIC__MODULE__VERSION "0:0:0"
+#define PACKAGENERIC__MODULE__DESCRIPTION "psycle open audio library"
+#define PACKAGENERIC__MODULE__SOURCE__PSYCLE__COAL__HELLO 1
