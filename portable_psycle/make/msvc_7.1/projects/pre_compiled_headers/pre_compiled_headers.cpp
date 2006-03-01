769d8907db6610074ca24bf58384072387a5ebb6
Johan Boule <johan.boule@retropaganda.info>
2006-03-01 19:34:36 +0000

    compilation fixes for msvc7.1 everything builds fine.
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1528 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- portable_psycle/make/msvc_7.1/projects/pre_compiled_headers/pre_compiled_headers.cpp
+++ portable_psycle/make/msvc_7.1/projects/pre_compiled_headers/pre_compiled_headers.cpp
@@ -1,3 +1,3 @@
 #include <project.private.hpp>
 #pragma hdrstop // borland and msvc: instructs the compiler to stop pre-compiling headers
-void pragma(dllexport) pre_compiled_headers() {} // a dummy function needed to force msvc to generate something
+//void pragma(dllexport) pre_compiled_headers() {} // a dummy function needed to force msvc to generate something
