86837ce733b174ec15d1be1e80f3b28bd6907e14
Johan Boule <johan.boule@retropaganda.info>
2006-03-19 04:23:04 +0000

    preparing stuff for gnu autotools
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1706 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ psycle/make/gnu_autotools/modules/bin.psycle.gui.cpp
@@ -0,0 +1,13 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// Copyright © 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+
+///\file
+///\brief
+#include PACKAGENERIC__PRE_COMPILED
+#include PACKAGENERIC
+#include <psycle/detail/project.private.hpp>
+#include <psycle/front_ends/gui/main.hpp>
+extern "C" int main(int /*const*/ argument_count, char /*const*/ * /*const*/ arguments[]) throw()
+{
+	return psycle::front_ends::gui::main(argument_count, arguments);
+}
