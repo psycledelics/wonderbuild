a43601f84ab7aa58ab9de7402dc0bc03cf884ae7
Johan Boule <johan.boule@retropaganda.info>
2006-03-20 22:20:15 +0000

    work in progress on autotools
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@1726 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/gnu_autotools/packageneric/package/modules/bin.psycle.text.cpp
+++ psycle/make/gnu_autotools/packageneric/package/modules/bin.psycle.text.cpp
@@ -1,13 +1,14 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright © 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+// copyright 2004-2006 Johan Boule <bohan@jabber.org>
+// copyright 2006 psycledelics http://psycle.pastnotecut.org
 
 ///\file
 ///\brief
 #include PACKAGENERIC__PRE_COMPILED
 #include PACKAGENERIC
 #include <psycle/detail/project.private.hpp>
-#include <psycle/front_ends/text/main.hpp>
+#include <psycle/player/main.hpp>
 extern "C" int main(int /*const*/ argument_count, char /*const*/ * /*const*/ arguments[]) throw()
 {
-	return psycle::front_ends::text::main(argument_count, arguments);
+	return psycle::player::main(argument_count, arguments);
 }
