02592a5c90d8e898f2023aaec15a5e001952dcc6
Johan Boule <johan.boule@retropaganda.info>
2006-04-28 19:28:29 +0000

    global fix on copyright years
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@2298 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/generic/detail/autotools/automake/package/modules/pre_compiled/bin.cpp
+++ packageneric/generic/detail/autotools/automake/package/modules/pre_compiled/bin.cpp
@@ -1,5 +1,5 @@
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// Copyright (C) 1999-2005 Psycledelics http://psycle.pastnotecut.org : Johan Boule
+// copyright 2000-2006 psycledelics http://psycle.pastnotecut.org : Johan Boule
 ///\meta generic
 #include PACKAGENERIC__PRE_COMPILED
 #include PACKAGENERIC
