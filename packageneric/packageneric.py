5a42f4f4cb02919abf9138a35e2c918f3be7deb0
Johan Boule <johan.boule@retropaganda.info>
2006-05-04 01:01:36 +0000

    packageneric python rewrite
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@2342 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/packageneric.py
+++ packageneric/packageneric.py
@@ -1,8 +1,76 @@
 #! /usr/bin/env python
 
+###################################################################
+###################################################################
+###################################################################
+
+sp = SourcePackage(
+	name = 'psycle',
+	version = [0, 0, 0]
+)
+
+m = Module(
+	name = 'lib-psycle.engine',
+	type = Module.types.lib,
+	version = [0, 0, 0]
+)
+
+m.requires.add(
+	debian = ['lib-universalis-dev >= 0'],
+	pkgconfig = ['universalis >= 0']
+)
+
+m.sources.add(find('src', 'psycle/engine', '*.cpp'))
+m.headers.add(find('src', 'psycle/engine', '*.hpp'))
+
+m.pkgconfig(
+	name = 'psycle.engine',
+	description = 'psycle engine library'
+)
+
+mp = ModulePackage(
+	name = 'psycle.plugins',
+	version = [0, 0, 0],
+	description = 'psycle plugins'
+)
+
+mp.add([psycle.plugin.sine, psycle.plugin.output.default])
+
+bp = binary_package(
+	name = 'lib-hello',
+	version = [0, 0, 0]
+	description = 'hi!'
+	long_description =
+		"""
+			hello, world!
+		"""
+)
+
+bp.add(mp.modules)
+bp.files.add(destination = os.path.join(bp.share, 'pixmaps', sp.name), find('pixmaps', '*.xpm'))
+
+bp_dev = binary_package('lib-hello-dev', [mp], version = [0, 0, 0])
+bp_dev.add([mp.headers, mp.pkgconfigs])
+
+da = distribution_archive('shell.sourceforge.net:/home/groups/p/ps/psycle/htdocs/packages/debian')
+da.add('sid', 'unstable', [sp, bp, bp_dev])
+
+###################################################################
+###################################################################
+###################################################################
+
 import os, fnmatch
 
-class GlobDirectoryWalker:
+class EnvList:
+	def __init__(self):
+		env = Environment()
+		dictionary = env.Dictionary()
+		keys = dictionary.keys()
+		keys.sort()
+		for key in keys:
+			print '%s = %s' % (key, dictionary[key])
+
+class Find:
 	# a forward iterator that traverses a directory tree
 	
 	def __init__(self, directory, pattern="*"):
@@ -29,22 +97,28 @@ class GlobDirectoryWalker:
 				if fnmatch.fnmatch(file, self.pattern):
 					return fullname
 
-class source
+class CPPDefine:
+	def __init(name, value = ''):
+		seff.name = name
+		self.value = value
+
+class Source:
 	def __init__(self, filename):
 		self.filename = filename
+		self.cppflags = ''
 
-class header
+class Header:
 	def __init__(self, filename):
 		self.filename = filename
 	
-class object
+class Object:
 	def __init__(self, source):
 		self.source = source
 	
 	def scons(self):
 		scons.Object(source)
 	
-class module
+class Module:
 	def __init__(self, name, type):
 		self.name = name
 		self.type = type
@@ -76,7 +150,7 @@ class module
 			result.append(x.merged_cppflags)
 		return result
 
-class module_package
+class ModulePackage
 	def __init__(self, name):
 		self.name = name
 		self.cflags = []
@@ -102,35 +176,12 @@ class module_package
 			result.append(x.merged_cppflags)
 		return result
 
-class binary_package
+class BinaryPackage
 	def __init__(self, name):
 		self.name = name
 		self.binary_version = 0
 		self.package_version = 0
 
-class distribution_archive
+class DistributionArchive
 	def __init__(self. remote_path):
 		self.remote_path = remote_path
-
-sp = source_package('psycle', version = [0, 0, 0])
-
-module m('lib-psycle.engine' lib, version = [0, 0, 0])
-m.requires.add(debian = ['lib-universalis-dev >= 0'], pkgconfig = ['universalis >= 0'])
-m.sources.add(find('src', 'psycle/engine', '*.cpp'))
-m.headers.add(find('src', 'psycle/engine', '*.hpp'))
-m.pkgconfig('psycle.engine', 'psycle engine library')
-
-mp = module_package('psycle.plugins', version = [0, 0, 0], 'psycle plugins')
-mp.add([psycle.plugin.sine, psycle.plugin.output.default])
-
-bp = binary_package('lib-hello', [mp], version = [0, 0, 0])
-bp.description('hi!')
-bp.long_description('hello, world!')
-bp.add(mp.modules)
-bp.files.add(destination = os.path.join(bp.share, 'pixmaps', sp.name), find('pixmaps', '*.xpm'))
-
-bp_dev = binary_package('lib-hello-dev', [mp], version = [0, 0, 0])
-bp_dev.add([mp.headers, mp.pkgconfigs])
-
-da = distribution_archive('shell.sourceforge.net:/home/groups/p/ps/psycle/htdocs/packages/debian')
-da.add('sid', 'unstable', [sp, bp, bp_dev])
