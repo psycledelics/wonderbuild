843368c1af34702a880ac1b4a64c5162c9d97de4
Johan Boule <johan.boule@retropaganda.info>
2006-05-08 13:48:28 +0000

    packageneric: work-in-progress on scons
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@2358 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/generic/__init__.py
+++ packageneric/generic/__init__.py
@@ -14,8 +14,73 @@ def pkg_config_check(context, name):
 	 context.Result(result)
 	 return result
 
+def print_all_nodes(dirnode, level=0):
+	"""Print all the scons nodes that are children of this node, recursively."""
+	if type(dirnode)==type(''):
+		dirnode=Dir(dirnode)
+	dt = type(Dir('.'))
+	for f in dirnode.all_children():
+		if type(f) == dt:
+			print "%s%s: .............."%(level * ' ', str(f))
+			print_dir(f, level+2)
+		print "%s%s"%(level * ' ', str(f))
+
+def Glob(includes = ['*'], excludes = None, dir = '.'):
+	"""Similar to glob.glob, except globs SCons nodes, and thus sees generated files and files from build directories.
+	Basically, it sees anything SCons knows about.
+	A key subtlety is that since this function operates on generated nodes as well as source nodes on the filesystem,
+	it needs to be called after builders that generate files you want to include.
+	
+	It will return both Dir entries and File entries
+	"""
+	
+	def fn_filter(node):
+		fn = os.path.basename(str(node))
+		match = False
+		for include in includes:
+			if fnmatch.fnmatchcase(fn, include):
+				match = True
+				break
+		if match and not excludes is None:
+			for exclude in excludes:
+				if fnmatch.fnmatchcase(fn, exclude):
+					match = False
+					break
+		return match
+
+	def filter_nodes(where):
+		children = filter(fn_filter, where.all_children(scan=0))
+		nodes = []
+		for f in children:
+			nodes.append(gen_node(f))
+		return nodes
+
+	def gen_node(n):
+		"""Checks first to see if the node is a file or a dir, then creates the appropriate node.
+		(code seems redundant, if the node is a node, then shouldn't it just be left as is?)
+		"""
+		if type(n) in (type(''), type(u'')):
+			path = n
+		else:
+			path = n.abspath
+		if os.path.isdir(path):
+			return Dir(n)
+		else:
+			return File(n)
+		
+	here = Dir(dir)
+	nodes = filter_nodes(here)
+	node_srcs = [n.srcnode() for n in nodes]
+	src = here.srcnode()
+	if src is not here:
+		for s in filter_nodes(src):
+			if s not in node_srcs:
+				# Probably need to check if this node is a directory
+				nodes.append(gen_node(os.path.join(dir, os.path.basename(str(s)))))
+	return nodes
+
 class Find:
-	# a forward iterator that traverses a directory tree
+	"""a forward iterator that traverses a directory tree"""
 	
 	def __init__(self, strip_path, path, pattern="*"):
 		self.strip_path = strip_path
@@ -39,7 +104,7 @@ class Find:
 				path = os.path.join(self.directory, file)
 				if os.path.isdir(os.path.join(self.strip_path, path)):
 					self.stack.append(path)
-				if fnmatch.fnmatch(file, self.pattern):
+				if fnmatch.fnmatchcase(file, self.pattern):
 					return path
 				
 class EnvList:
