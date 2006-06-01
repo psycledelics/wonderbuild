6bf08de9d5690288e97f6bda78812205ab517a36
Johan Boule <johan.boule@retropaganda.info>
2006-06-01 01:01:57 +0000

    packageneric: scons
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@2706 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/generic/pkg_config_package.py
+++ packageneric/generic/pkg_config_package.py
@@ -15,7 +15,7 @@ class pkg_config_package:
 			self._modules = []
 		else:
 			self._modules = modules
-			
+		
 	def packageneric(self):
 		return self._packageneric
 		
@@ -38,3 +38,38 @@ class pkg_config_package:
 				if not package in packages:
 					packages.append(package)
 		return packages
+
+	def pc(self):
+		'''
+		prefix=/home/bohan/work/dev/++packageneric/install
+		exec_prefix=${prefix}
+		pkgversion=0.0.20060512
+		pkgnameversion=universalis-${pkgversion}
+		pkgversionlibdir=${exec_prefix}/lib/${pkgnameversion}
+		pkgversionincludedir=${prefix}/include/${pkgnameversion}
+		pkgversionsharedincludedir=${prefix}/share/include/${pkgnameversion}
+
+		Name: universalis 0:0:0 i686-pc-linux-gnu
+		Description: universalis platform abstraction layer
+		Version: ${pkgversion}
+		Cflags: -I${prefix}/include -I${pkgversionincludedir}   
+		Libs: -L${exec_prefix}/lib -L${pkgversionlibdir} -l-universalis  -lboost_thread -lboost_filesystem -lboost_signals 
+		Requires:  diversalis >= 0.0.0 glibmm-2.4 >= 2.4 gthread-2.0 >= 2.0
+		'''
+		string = 'prefix=' + self.packageneric().environment().subst('$PREFIX') + '\n'
+		string += 'Name: ' + self.name() + ' ' + str(self.version()) + '\n'
+		string += 'Description: ' + self.description() + '\n'
+		string += 'Version: ' + str(self.version()) + '\n' # todo module *interface* version, not package one
+		string += 'CFlags: ' + self.packageneric().environment().subst('$CXXFLAGS') + '\n'
+		string += 'Libs: ' + self.packageneric().environment().subst('$LINKFLAGS')
+		for library in self.packageneric().environment().Split(self.packageneric().environment().subst('$LIBS')):
+			string += ' -l' + library
+		string += '\n'
+		string += 'Libs.private: ' + '\n'
+		string += 'Requires: '
+		for package in self.build_depends():
+			if package.pkg_config():
+				string += package.pkg_config() + ', '
+		string += '\n'
+		#self.packageneric().environment().Alias(self.name() + '.pc', ...)
+		return string
