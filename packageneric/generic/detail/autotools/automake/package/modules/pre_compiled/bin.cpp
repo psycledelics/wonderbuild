f1ba180ada704c24d0571ec27870983fe1063f17
Johan Boule <johan.boule@retropaganda.info>
2007-01-12 18:55:42 +0000

    removed the old autotools files
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@3804 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/generic/detail/autotools/automake/package/modules/pre_compiled/bin.cpp
+++
@@ -1,6 +0,0 @@
-// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 2000-2006 psycledelics http://psycle.pastnotecut.org : Johan Boule
-///\meta generic
-#include PACKAGENERIC__PRE_COMPILED
-#include PACKAGENERIC
-#include "pre-compiled-headers.private.hpp"
