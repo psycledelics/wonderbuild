d2210e625c92609de43279687d7c8ea9ab084edc
jaz001 <jaz001>
2007-07-17 21:10:22 +0000

    Plugin compilation fixes (added missing plugins, missing sources, adapted universalis/diversalis from trunk to 1.8)
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/psycle-1.8-bugfixes@5149 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_7.1/projects/pre_compiled_headers/pre_compiled_headers.cpp
+++ psycle/make/msvc_7.1/projects/pre_compiled_headers/pre_compiled_headers.cpp
@@ -1,3 +1,4 @@
-#include <project.private.hpp>
+#include <packageneric/pre-compiled.private.hpp>
 #pragma hdrstop // borland and msvc: instructs the compiler to stop pre-compiling headers
-void pragma(dllexport) pre_compiled_headers() {} // a dummy function needed to force msvc to generate something
+//void pragma(dllexport) pre_compiled_headers() {} // a dummy function needed to force msvc to generate something
+int i=0;
---
+++ psycle/src/packageneric/project.private.hpp
@@ -0,0 +1,18 @@
+// This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+// copyright 1999-2007 johan boule <bohan@jabber.org>
+// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+
+///\file
+#pragma once
+#error packageneric
+#include <packageneric/stringized.private.hpp>
+
+#define PACKAGENERIC__PACKAGE__VERSION \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__PACKAGE__VERSION__MAJOR) "." \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__PACKAGE__VERSION__MINOR) "." \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__PACKAGE__VERSION__PATCH)
+
+#define PACKAGENERIC__MODULE__VERSION \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__MODULE__VERSION__INTERFACE__MININUM_COMPATIBLE) "." \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__MODULE__VERSION__INTERFACE) "." \
+	PACKAGENERIC__STRINGIZED(PACKAGENERIC__MODULE__VERSION__IMPLEMENTATION)
