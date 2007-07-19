c465d12962a9f657004481ef84adf0a4af7e92a9
Johan Boule <johan.boule@retropaganda.info>
2007-07-19 21:31:47 +0000

    copied external includes from trunk (including boost 1.33 for msvc8)
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/psycle-1.8-bugfixes@5166 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_7.1/projects/boost/build.cmd
+++ psycle/make/msvc_7.1/projects/boost/build.cmd
@@ -1,13 +1,19 @@
+@echo off
+
+%~d0
+cd %~p0
+
 pushd ..\..\..\..\include\ && (
 	if not exist boost (
-		boost -y
+		boost-1.32 -y || exit /b 1
 	)
 	popd
 ) && (
 	if not exist ..\..\output\boost_stamp (
 		pushd ..\..\ && (
-			output.boost -y
-			echo boost extracted > output\boost_stamp
+			output.boost -y || exit /b 1
+			echo boost extracted > output\boost_stamp || exit /b 1
+			popd
 		)
 	)
 )
--- psycle/make/msvc_7.1/projects/direct_sound/clean.cmd
+++ psycle/make/msvc_7.1/projects/direct_sound/clean.cmd
@@ -13,24 +13,24 @@ cd %~p0
 
 pushd ..\..\output && (
 	rem [bohan] delete the stamp file in any case, because the rest sometimes fails for unknown reasons
-	del/q boost_stamp || exit /b 1
+	del/q direct_sound_stamp || exit /b 1
 	popd
 ) && (
 	if exist ..\..\output (
 		pushd ..\..\output && (
-			del/s/q boost_* || exit /b 1
-			del/s/q libboost_* || exit /b 1
+			del/s/q direct_sound_stamp || exit /b 1
+			del/s/q dsound.lib || exit /b 1
 			popd
 		)
 	)
 ) && (
 	pushd ..\..\..\..\include\ && (
-		if exist boost (
+		if exist dsound.h (
 			rem [bohan] i don't know why sometimes it fails
 			rem [bohan] it seems we need to do it 5 times ...
-			rmdir/s/q boost || exit /b 1
+			rmdir/s/q dsound.h || exit /b 1
 			rem [bohan] i don't know why sometimes it doesn't report the failure
-			if exist boost exit /b 1
+			if exist dsound.h exit /b 1
 		)
 		popd
 	)
