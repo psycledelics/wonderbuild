525a03baa51dbf612ec3ea0c693925944d05dce8
Johan Boule <johan.boule@retropaganda.info>
2007-07-22 19:14:51 +0000

    moving files to a common place
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/psycle-1.8-bugfixes@5193 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_7.1/projects/asio/src/asio.cpp
+++
@@ -1,3 +0,0 @@
-#include <packageneric/pre-compiled.private.hpp>
-#include "asio.private.hpp"
-#include <asio/common/asio.cpp>
--- psycle/make/msvc_7.1/projects/asio/src/asiodrivers.cpp
+++
@@ -1,3 +0,0 @@
-#include <packageneric/pre-compiled.private.hpp>
-#include "asio.private.hpp"
-#include <asio/host/asiodrivers.cpp>
