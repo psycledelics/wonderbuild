7aa47aca42e86375356e9fb80031c5e4002a3fd4
Johan Boule <johan.boule@retropaganda.info>
2007-08-12 15:21:57 +0000

    use the external-packages/zlib-1.2.3 dir for msvc7.1
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5424 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ psycle/make/msvc_7.1/projects/zlib/build.cmd
@@ -0,0 +1,39 @@
+@echo off
+
+%~d0
+cd %~p0
+
+set pkgdir=..\..\..\..\..\external-packages\zlib-1.2.3
+set libdir=lib-mswindows-cabi
+
+if not exist ..\..\..\..\include\zlib.h (
+	if not exist ..\..\..\..\include (
+		mkdir ..\..\..\..\include || exit /b 1
+	)
+	xcopy/f/i %pkgdir%\include\*.h ..\..\..\..\include\ || exit /b 1
+)
+
+if not exist ..\..\output\zlib_stamp (
+
+	if not exist ..\..\output\debug\bin (
+		mkdir ..\..\output\debug\bin || exit /b 1
+	)
+	xcopy/f %pkgdir%\%libdir%\zlib1.dll ..\..\output\debug\bin\ || exit /b 1
+
+	if not exist ..\..\output\debug\lib (
+		mkdir ..\..\output\debug\lib || exit /b 1
+	)
+	xcopy/f %pkgdir%\%libdir%\zlib.lib ..\..\output\debug\lib\ || exit /b 1
+
+	if not exist ..\..\output\release\bin (
+		mkdir ..\..\output\release\bin || exit /b 1
+	)
+	xcopy/f %pkgdir%\%libdir%\zlib1.dll ..\..\output\release\bin\ || exit /b 1
+
+	if not exist ..\..\output\release\lib (
+		mkdir ..\..\output\release\lib || exit /b 1
+	)
+	xcopy/f %pkgdir%\%libdir%\zlib.lib ..\..\output\release\lib\ || exit /b 1
+
+	echo zlib copied > ..\..\output\zlib_stamp || exit /b 1
+)
---
+++ psycle/make/msvc_7.1/projects/zlib/clean.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+
+rem ================================================================================================
+rem disabled because there's no way to make it work on windows.. file and or dirs seems to be locked
+goto :eof
+rem ================================================================================================
+
+
+
+%~d0
+cd %~p0
+
+pushd ..\..\output && (
+	rem [bohan] delete the stamp file in any case, because the rest sometimes fails for unknown reasons
+	del/q zlib_stamp || exit /b 1
+	popd
+) && (
+	if exist ..\..\output (
+		pushd ..\..\output && (
+			del/s/q zlib_stamp || exit /b 1
+			del/s/q zlib.lib || exit /b 1
+			del/s/q zlib1.dll || exit /b 1
+			popd
+		)
+	)
+)
