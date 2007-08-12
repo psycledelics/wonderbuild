8c71bb6f50d479bdf7c971eccf797fff462b9fa8
Johan Boule <johan.boule@retropaganda.info>
2007-08-12 14:09:45 +0000

    use the external-packages/boost-1.33.1 dir for msvc8
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5417 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_8.0/projects/boost/build.cmd
+++ psycle/make/msvc_8.0/projects/boost/build.cmd
@@ -3,17 +3,50 @@
 %~d0
 cd %~p0
 
-pushd ..\..\..\..\include\ && (
-	if not exist boost (
-		boost-1.33.1 -y || exit /b 1
+set pkgdir=..\..\..\..\..\external-packages\boost-1.33.1\
+set libdir=lib-mswindows-msvc-8.0-cxxabi-1400
+
+if not exist ..\..\..\..\include\boost (
+	pushd %pkgdir% || exit /b 1
+	..\7za\7za x -y include.tar.bz2 || exit /b 1
+	..\7za\7za x -y include.tar || exit /b 1
+	del/q include.tar || exit /b 1
+	popd || exit /b 1
+
+	if not exist ..\..\..\..\include\ (
+		mkdir ..\..\..\..\include\ || exit /b 1
 	)
-	popd
-) && (
-	if not exist ..\..\output\boost_stamp (
-		pushd ..\..\ && (
-			output.boost -y || exit /b 1
-			echo boost extracted > output\boost_stamp || exit /b 1
-			popd
-		)
+	move %pkgdir%\include\boost ..\..\..\..\include\boost || exit /b 1
+	rmdir/s/q %pkgdir%\include || exit /b 1
+)
+
+if not exist ..\..\output\boost_stamp (
+	pushd %pkgdir% || exit /b 1
+	..\7za\7za x -y %libdir%.tar.bz2 || exit /b 1
+	..\7za\7za x -y %libdir%.tar || exit /b 1
+	del/q %libdir%.tar || exit /b 1
+	popd || exit /b 1
+
+	if not exist ..\..\output\debug\bin (
+		mkdir ..\..\output\debug\bin || exit /b 1
 	)
+	move %pkgdir%\%libdir%\*-gd-*.dll ..\..\output\debug\bin || exit /b 1
+
+	if not exist ..\..\output\debug\lib (
+		mkdir ..\..\output\debug\lib || exit /b 1
+	)
+	move %pkgdir%\%libdir%\*-gd-*.lib ..\..\output\debug\lib || exit /b 1
+
+	if not exist ..\..\output\release\bin (
+		mkdir ..\..\output\release\bin || exit /b 1
+	)
+	move %pkgdir%\%libdir%\*.dll ..\..\output\release\bin || exit /b 1
+
+	if not exist ..\..\output\release\lib (
+		mkdir ..\..\output\release\lib || exit /b 1
+	)
+	move %pkgdir%\%libdir%\*.lib ..\..\output\release\lib || exit /b 1
+
+	echo boost extracted > ..\..\output\boost_stamp || exit /b 1
+	rmdir/s/q %pkgdir%\%libdir% || exit /b 1
 )
