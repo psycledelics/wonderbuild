49e0f4c3a0e259cd107a98ea6f26f15c431b04b5
Johan Boule <johan.boule@retropaganda.info>
2007-08-18 18:03:11 +0000

    log/report for the tests
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5504 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- buildbot/master/configuration.py
+++ buildbot/master/configuration.py
@@ -207,7 +207,7 @@ BuildmasterConfig['builders'].append(
 			[
 				factory.s(step.SVN, mode = 'update', svnurl = svn_url, locks = [svn_lock]),
 				factory.s(step.Compile, command = 'scons --directory=psycle-helpers packageneric__debug=0 packageneric__test=1', locks = [compile_lock]),
-				factory.s(step.Test, command = './++packageneric/variants/default/stage-install/usr/local/bin/psycle-helpers_unit_tests', locks = [compile_lock])
+				factory.s(step.Test, command = './++packageneric/variants/default/stage-install/usr/local/bin/psycle-helpers_unit_tests --log_level=test_suite --report_level=detailed', locks = [compile_lock])
 			]
 		)
 	}
