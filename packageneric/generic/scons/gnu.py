4201b1964c48196621049ded6b1b812ea3211c8c
Johan Boule <johan.boule@retropaganda.info>
2007-08-23 22:06:39 +0000

    gcc __k8__ and __pentium4__ etc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5537 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/generic/scons/gnu.py
+++ packageneric/generic/scons/gnu.py
@@ -267,9 +267,12 @@ def implementation(chain):
 			#'-Wpadded',
 			#'-Winline', # warns about explicit inline functions not inlined
 			'-Wdisabled-optimization',
-			#'-mfpmath=sse', # todo need to check for x86 or is it ignored otherwise?
+			'-march=k8', # implies -mtune=k8, moreover, use specific instruction set
+			'-mfpmath=sse', # todo need to check for x86 or is it ignored otherwise?
+			#'-msse,387' # experimental!
 			#'-msse' # -msse2 is implicit on 64-bit platforms
-			#'-msse2' # -msse2 is implicit on 64-bit platforms
+			'-msse2' # -msse2 is implicit on 64-bit platforms
+			#-ffast-math
 		])
 		chain.linker().flags().add([
 			'-Wl,-O3',
