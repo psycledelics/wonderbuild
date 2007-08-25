2bc7ed6e3a989004730670a856fde7ba81b75de4
Johan Boule <johan.boule@retropaganda.info>
2007-08-25 23:25:34 +0000

    new ms tools are ALL BROKEN, so the script has no choice but to call DevEnv :-(
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5567 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_8.0/build.cmd
+++ psycle/make/msvc_8.0/build.cmd
@@ -6,4 +6,5 @@ if "%configuration%" == "" (
 )
 call "%VS80ComnTools%\VSVars32" || exit /b 1
 rem msbuild solution.sln -property:configuration=%configuration% || exit /b 1
-vcbuild solution.sln "%configuration%|Win32" || exit /b 1
+rem vcbuild solution.sln "%configuration%|Win32" || exit /b 1
+DevEnv solution.sln /build %configuration% || exit /b 1
