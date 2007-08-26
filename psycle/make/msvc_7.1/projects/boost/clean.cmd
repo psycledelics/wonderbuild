07863b4744220d1ced74630c1a9cb8db6382a69f
Johan Boule <johan.boule@retropaganda.info>
2007-08-26 23:42:29 +0000

    clean commands
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@5585 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_7.1/projects/boost/clean.cmd
+++ psycle/make/msvc_7.1/projects/boost/clean.cmd
@@ -28,7 +28,7 @@ pushd ..\..\output && (
 		if exist boost (
 			rem [bohan] i don't know why sometimes it fails
 			rem [bohan] it seems we need to do it 5 times ...
-			rd/s/q boost || exit /b 1
+			rmdir/s/q boost || exit /b 1
 			rem [bohan] i don't know why sometimes it doesn't report the failure
 			if exist boost exit /b 1
 		)
