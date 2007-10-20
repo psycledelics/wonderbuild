03331e9ca923696b9d884f215c0e38a6961e4fbe
Johan Boule <johan.boule@retropaganda.info>
2007-10-20 10:13:36 +0000

    svn:externals to build on windows
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/core-devel@5878 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ msvc/msvc_8.0/projects/psycle-helpers/src/packageneric/module.private.hpp
@@ -0,0 +1,11 @@
+///\file
+///\brief packageneric configuration for module psycoal
+#pragma once
+#include <packageneric/configuration.private.hpp>
+
+#define PACKAGENERIC__MODULE__NAME "psycle-helpers.unit-tests"
+#define PACKAGENERIC__MODULE__VERSION__INTERFACE 0
+#define PACKAGENERIC__MODULE__VERSION__INTERFACE__MININUM_COMPATIBLE 0
+#define PACKAGENERIC__MODULE__VERSION__IMPLEMENTATION 0
+#define PACKAGENERIC__MODULE__DESCRIPTION "unit tests of the psycle helper routines"
+#define PACKAGENERIC__MODULE__SOURCE__PSYCLE__HELPERS 1
