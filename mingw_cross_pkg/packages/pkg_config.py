241dd706e92b5f89657aae7f5bfeb0f165bb0eda
Johan Boule <johan.boule@retropaganda.info>
2008-04-21 16:29:54 +0000

    added pkg-config
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@6634 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- mingw_cross_pkg/packages/pkg_config.py
+++ mingw_cross_pkg/packages/pkg_config.py
@@ -10,6 +10,29 @@ class PkgConfig(Package):
 
 	def description(self): return 'manage compile and link flags for libraries'
 	
-	def download(self): self.http_get('pkgconfig.freedesktop.org/releases/pkg-config-' + self.version() + '.tar.gz')
+	def _tarball(self): return self._dir() + '.tar.gz'
+	def _dir(self): return 'pkg-config-' + self.version()
 
+	def download(self): self.http_get('pkgconfig.freedesktop.org/releases/' + self._tarball())
+
+	def clean_download(self): os.unlink(self._tarball())
+	
+	def build(self):
+		self.shell('tar xzf ' + self._tarball())
+		self.shell(
+			'cd ' + self._dir() + ' && \n'
+			'./configure \\\n'
+				'--prefix=' + os.path.join(self.prefix(), self.target())
+		)
+		self.continue_build()
+
+	def continue_build(self):
+		self.shell(self.gmake() + ' -C ' + self._dir() + ' install DESTDIR=' + self.dest_dir())
+		self.shell('install -d ' + os.path.join(self.prefix(), 'bin')
+		f = os.path.join(self.prefix(), 'bin', self.target() + '-pkg-config'
+		if os.path.exists(f): os.unlink(f)
+		self.shell('ln -s ' + os.path.join(os.pardir, self.target(), 'bin', 'pkg-config') + ' ' + f)
+	
+	def clean_build(self):
+		self.shell('rm -Rf ' + self._dir())
 
