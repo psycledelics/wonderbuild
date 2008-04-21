c895262aec0cb08d609fcae55dc55f367e916c4d
Johan Boule <johan.boule@retropaganda.info>
2008-04-21 13:40:21 +0000

    added zlib package
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@6630 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- tests/resconsible/cross_compile/packages/zlib.py
+++ tests/resconsible/cross_compile/packages/zlib.py
@@ -9,6 +9,30 @@ class ZLib(Package):
 		self.add_dep('gcc')
 
 	def description(self): return 'compression library'
+
+	def _tarball(self): return self._dir() + '.tar.bz2'
+	def _dir(self): return self.name() + '-' + self.version()
 	
-	def download(self): self.http_get(self.mirror('sourceforge') + '/libpng/zlib-' + self.version() + '.tar.bz2')
+	def download(self): self.http_get(self.mirror('sourceforge') + '/libpng/' + self._tarball())
+
+	def clean_download(self): os.unlink(self._tarball())
+	
+	def build(self):
+		self.shell('tar xjf ' + self._tarball())
+		self.shell(
+			'cd ' + self._dir() + ' && \n'
+			# zlib doesn't use autotools and its makefile doesn't support destdir.
+			# We cheat by setting prefix to destdir (it's ok since there's no rpath).
+			'CC=' + self.target() + '-gcc \\\n'
+			'RANLIB=' + self.target() + '-ranlib \\\n'
+			'./configure \\\n'
+				'--prefix=' + os.path.join(self.dest_dir() + self.prefix(), self.target())
+		)
+		self.continue_build()
+
+	def continue_build(self):
+		self.shell(self.gmake() + ' -C ' + self._dir() + ' install')
+	
+	def clean_build(self):
+		self.shell('rm -Rf ' + self._dir())
 
