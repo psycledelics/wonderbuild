797bd25160bf74d8fa8db302d433503a519d2593
Johan Boule <johan.boule@retropaganda.info>
2008-04-24 04:18:12 +0000

    cosmetics
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@6665 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- mingw_cross_pkg/packages/__init__.py
+++ mingw_cross_pkg/packages/__init__.py
@@ -274,8 +274,8 @@ class Packages:
 						if False: shutil.copytree(os.curdir, self._prefix, symlinks = True)
 							# note: shutil.copytree is not as good (rated "example code" in the documentation)
 						else: self.shell('cp -R * ' + self._prefix, verbose = False)
-						for f in ['version', 'description', 'direct-dependencies', 'recursed-dependencies']:
-							shutil.copy(os.path.join(built_package.state_dir(), f), installed_package.state_dir())
+						for state_name in ['version', 'description', 'direct-dependencies', 'recursed-dependencies']:
+							shutil.copy(built_package.state_dir(state_name), installed_package.state_dir())
 						try: del self._packages[package.name()]
 						except KeyError: pass
 				installed_package.write_state('installed', installed)
