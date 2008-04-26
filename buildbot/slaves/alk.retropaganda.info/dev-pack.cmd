d33faa6fcd224138f630a38b7f16da95bbd4233b
Johan Boule <johan.boule@retropaganda.info>
2008-04-26 11:50:59 +0000

    builder dirs
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@6691 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ buildbot/slaves/alk.retropaganda.info/dev-pack.cmd
@@ -0,0 +1,36 @@
+set LIB=
+set INCLUDE=
+
+if "%2" neq "msvc-solution" (
+	call %~dp0\..\psycle-dev-pack-for-mswindows\libexec\main %* || exit /b 1
+	goto :msvc_extra
+)
+
+:msvc_hardcoded
+	rem hardcoded paths for unintelligent .sln/.vcproj/.vcprops stuff
+
+	set X=%~dp0\..\psycle-dev-pack-for-mswindows\opt\gtk
+
+	set LIB=%LIB%;%X%\lib
+	set INCLUDE=%INCLUDE%;%X%\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\libxml++-2.6
+	set INCLUDE=%INCLUDE%;%X%\lib\libxml++-2.6\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\libxml2
+
+	set INCLUDE=%INCLUDE%;%X%\include\glibmm-2.4
+	set INCLUDE=%INCLUDE%;%X%\lib\glibmm-2.4\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\sigc++-2.0
+	set INCLUDE=%INCLUDE%;%X%\lib\sigc++-2.0\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\glib-2.0
+	set INCLUDE=%INCLUDE%;%X%\lib\glib-2.0\include
+)
+
+:msvc_extra
+	rem msvc libs not in the dev-pack
+	
+	set INCLUDE=%INCLUDE%;x:\usr\lib\dx\include
+	set LIB=%LIB%;x:\usr\lib\dx\lib
