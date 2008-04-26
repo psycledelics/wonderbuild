71df9625ba22898b24489c4b05ae2d9850ca87cc
Johan Boule <johan.boule@retropaganda.info>
2008-04-26 11:49:55 +0000

    dev-pack
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@6690 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- buildbot/slaves/winux.retropaganda.info/dev-pack.cmd
+++ buildbot/slaves/winux.retropaganda.info/dev-pack.cmd
@@ -1,8 +1,36 @@
-call %~dp0\..\psycle-dev-pack-for-mswindows\libexec\main no-msys || exit /b 1
+set LIB=
+set INCLUDE=
 
-if "%1" == "scons-tools-mingw" set SCONS_TOOLS=mingw || exit /b 1
+if "%2" neq "msvc-solution" (
+	call %~dp0\..\psycle-dev-pack-for-mswindows\libexec\main %* || exit /b 1
+	goto :msvc_extra
+)
 
-rem if "%1" == "scons-tools-msvc" (
+:msvc_hardcoded
+	rem hardcoded paths for unintelligent .sln/.vcproj/.vcprops stuff
+
+	set X=%~dp0\..\psycle-dev-pack-for-mswindows\opt\gtk
+
+	set LIB=%LIB%;%X%\lib
+	set INCLUDE=%INCLUDE%;%X%\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\libxml++-2.6
+	set INCLUDE=%INCLUDE%;%X%\lib\libxml++-2.6\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\libxml2
+
+	set INCLUDE=%INCLUDE%;%X%\include\glibmm-2.4
+	set INCLUDE=%INCLUDE%;%X%\lib\glibmm-2.4\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\sigc++-2.0
+	set INCLUDE=%INCLUDE%;%X%\lib\sigc++-2.0\include
+
+	set INCLUDE=%INCLUDE%;%X%\include\glib-2.0
+	set INCLUDE=%INCLUDE%;%X%\lib\glib-2.0\include
+)
+
+:msvc_extra
+	rem msvc libs not in the dev-pack
+	
 	set INCLUDE=%INCLUDE%;x:\usr\lib\dx\include
 	set LIB=%LIB%;x:\usr\lib\dx\lib
-rem )
