ebce4590f6d4e793203ae1ac866fa1cac7d29c08
Johan Boule <johan.boule@retropaganda.info>
2008-05-03 23:07:33 +0000

    remove dest dir before build
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@6829 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- mingw_cross_pkg/packages/__init__.py
+++ mingw_cross_pkg/packages/__init__.py
@@ -269,7 +269,7 @@ class Packages:
 						built_package.make_state_dir('build')
 						self._dest_dir = built_package.state_dir('dest') # transmit dest dir to package
 						if not continue_build:
-							if os.path.exists(self._dest_dir): shutil.rmtree(self._dest_dir)
+							if os.path.exists(built_package.state_dir('dest')): shutil.rmtree(built_package.state_dir('dest'))
 							if not skip_download:
 								os.chdir(built_package.state_dir('build'))
 								package_recipe.download()
