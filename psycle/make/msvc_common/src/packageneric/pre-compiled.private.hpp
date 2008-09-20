86b223f274397520a44954d10a14d3ffa98b9d19
Johan Boule <johan.boule@retropaganda.info>
2008-09-20 22:01:05 +0000

    even more stupid stuff
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@7122 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_common/src/packageneric/pre-compiled.private.hpp
+++ psycle/make/msvc_common/src/packageneric/pre-compiled.private.hpp
@@ -214,7 +214,52 @@
 /******************************************************************************************************************/
 // boost http://boost.org
 
-#include "boost.private.hpp"
+#if defined DIVERSALIS__COMPILER__MICROSOFT
+	#pragma message("packageneric::pre_compiled:: parsing boost headers")
+#endif
+
+#include <boost/version.hpp>
+#include <boost/static_assert.hpp>
+//#include "boost/multi_array.hpp"
+#include <boost/filesystem/path.hpp>
+
+#define BOOST_THREAD_USE_DLL
+#include <boost/thread/thread.hpp>
+#include <boost/thread/condition.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/recursive_mutex.hpp>
+#if BOOST_VERSION == 103200
+	#include <boost/thread/read_write_mutex.hpp>
+#endif
+
+// huge include! #include <boost/spirit.hpp>
+
+#if BOOST_VERSION >= 103301
+	//#include <boost/archive/text_iarchive.hpp>
+	//#include <boost/archive/text_oarchive.hpp>
+	#include <boost/archive/xml_iarchive.hpp>
+	#include <boost/archive/xml_oarchive.hpp>
+	#if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
+		#pragma warning(push)
+		#pragma warning(disable:4267) // 'argument' : conversion from 'size_t' to 'std::streamsize', possible loss of data
+	#endif
+	//#include <boost/archive/binary_iarchive.hpp>
+	//#include <boost/archive/binary_oarchive.hpp>
+	#if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
+		#pragma warning(pop)
+	#endif
+	//#include <boost/serialization/level.hpp>
+	//#include <boost/serialization/version.hpp>
+	//#include <boost/serialization/tracking.hpp>
+	//#include <boost/serialization/export.hpp>
+	#include <boost/serialization/nvp.hpp>
+	//#include <boost/serialization/list.hpp>
+	#include <boost/serialization/string.hpp>
+#endif
+
+#if defined DIVERSALIS__COMPILER__MICROSOFT
+	#pragma message("packageneric::pre_compiled:: done parsing boost headers")
+#endif
 
 /******************************************************************************************************************/
 
