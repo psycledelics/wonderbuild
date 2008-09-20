d916f2b35da62096e567cc61cb579b102a78f823
Johan Boule <johan.boule@retropaganda.info>
2008-09-20 21:30:32 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@7117 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/src/packageneric/pre-compiled.private.hpp
+++ psycle/src/packageneric/pre-compiled.private.hpp
@@ -1,83 +1,40 @@
 // -*- mode:c++; indent-tabs-mode:t -*-
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
-// copyright 1999-2007 johan boule <bohan@jabber.org>
-// copyright 2004-2007 psycledelics http://psycle.pastnotecut.org
+// copyright 1999-2008 members of the psycle project http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
-///\file
-///\brief inclusions of headers which must be pre-compiled.
+///\file \brief inclusions of headers to be pre-compiled.
+
+#ifndef PACKAGENERIC__PRE_COMPILED__INCLUDED
+#define PACKAGENERIC__PRE_COMPILED__INCLUDED
 #pragma once
-#if defined PACKAGENERIC__PRE_COMPILED__INCLUDED
-	#error pre-compiled headers already included
-#else
-	#define PACKAGENERIC__PRE_COMPILED__INCLUDED
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-#include <diversalis/compiler.hpp> // defines DIVERSALIS__COMPILER__xxx
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
+#include <diversalis/compiler.hpp>
 #if defined DIVERSALIS__COMPILER__FEATURE__PRE_COMPILATION // if the compiler supports pre-compilation
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+/******************************************************************************************************************/
 
 #if defined DIVERSALIS__COMPILER__MICROSOFT
 	#pragma message("packageneric::pre_compiled:: parsing " __FILE__)
 #endif
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-#include <diversalis/diversalis.hpp> // defines DIVERSALIS__COMPILER__xxx and DIVERSALIS__OPERATING_SYSTEM__xxx
-#include <universalis/compiler.hpp> // includes warning settings
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+#include <diversalis/diversalis.hpp>
+#include <universalis/compiler.hpp>
 
 #if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION < 1300
 	#pragma warning(push, 3) // msvc6's standard libraries generate level-4 warnings... hmm, no comment
 #endif
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-///////////////
+/******************************************************************************************************************/
 // os-specific
-///////////////
 
 #if defined DIVERSALIS__COMPILER__MICROSOFT
 	#pragma message("packageneric::pre_compiled:: parsing operating-system specific headers")
 #endif
+
 #if defined DIVERSALIS__OPERATING_SYSTEM__MICROSOFT
 	#if defined DIVERSALIS__COMPILER__MICROSOFT
 		#pragma warning(push) // don't let microsoft mess around with our warning settings
 	#endif
+
 	#if !defined WIN32_LEAN_AND_MEAN
 		/// excludes rarely used stuff from windows headers.
 		/// beware,
@@ -85,14 +42,17 @@
 		/// when including <mmsystem.h>, WAVEFORMATEX is not declared
 		#define WIN32_LEAN_AND_MEAN
 	#endif
+
 	#if !defined VC_EXTRA_LEAN
 		/// excludes some more of the rarely used stuff from windows headers.
 		#define VC_EXTRA_LEAN
 	#endif
+
 	#if !defined NOMINMAX
 		/// tells microsoft's headers not to pollute the global namespace with min and max macros (which break a lot of libraries, including the standard c++ library!)
 		#define NOMINMAX
 	#endif
+
 	#if defined _AFXDLL // when mfc is used
 		#if defined DIVERSALIS__COMPILER__MICROSOFT
 			#pragma message("packageneric::pre_compiled:: parsing mfc headers")
@@ -116,24 +76,31 @@
 		#endif
 		#include <windows.h>
 	#endif
+
 	#if 1 || !defined NOMINMAX
 		// The following two mswindows macros break a lot of libraries, including the standard c++ library!
 		#undef min
 		#undef max
 	#endif
+
 	// gdi+, must be included after <windows.h> or <afxwin.h>
-	#if defined DIVERSALIS__COMPILER__MICROSOFT ///\todo is gdi+ available with other compilers than microsoft's? by default, it's safer to assume it's not, as usual.
+	///\todo is gdi+ available with other compilers than microsoft's? by default, it's safer to assume it's not, as usual.
+	#if defined DIVERSALIS__COMPILER__MICROSOFT
 		#if !defined min || !defined max // gdi+ needs min and max in the root namespace :-(
-			// the standard c++ header <algorithm> defines std::min and std::max, but microsoft named them __cpp_min and __cpp_max, prefering their own macro stuff rather than the standard
+			// the standard c++ header <algorithm> defines std::min and std::max,
+			// but microsoft named them __cpp_min and __cpp_max, prefering their own macro stuff rather than the standard
+
 			#if !defined min
 				/// replacement for mswindows' min macro, but using a template in the root namespace instead of the ubiquitous macro, so we prevent clashes.
 				template<typename X> X const inline & min(X const & x1, X const & x2) { return x1 < x2 ? x1 : x2; }
 			#endif
+
 			#if !defined max
 				/// replacement for mswindows' max macro, but using a template in the root namespace instead of the ubiquitous macro, so we prevent clashes.
 				template<typename X> X const inline & max(X const & x1, X const & x2) { return x1 > x2 ? x1 : x2; }
 			#endif
 		#endif
+
 		#if defined _AFXDLL // when mfc is used, also include <gdiplus.h>
 			//#include <atlbase.h> // for MIDL_INTERFACE used by <gdiplus.h>
 			#include <gdiplus.h>
@@ -142,19 +109,18 @@
 			#endif
 		#endif
 	#endif
+
 	#if defined DIVERSALIS__COMPILER__MICROSOFT
 		#pragma warning(pop) // don't let microsoft mess around with our warning settings
 	#endif
 #endif
+
 #if defined DIVERSALIS__COMPILER__MICROSOFT
 	#pragma message("packageneric::pre_compiled:: done parsing operating-system specific headers")
 #endif
 
-
-
-////////////////
+/******************************************************************************************************************/
 // c++ standard
-////////////////
 
 #if defined DIVERSALIS__COMPILER__MICROSOFT
 	#pragma message("packageneric::pre_compiled:: parsing standard c++ headers")
@@ -230,101 +196,35 @@
 	#pragma message("packageneric::pre_compiled:: done parsing standard c++ headers")
 #endif
 
-
-
-/////////
-// posix
-/////////
+/******************************************************************************************************************/
+// posix standard
 
 #if defined DIVERSALIS__OPERATING_SYSTEM__POSIX
 	#if defined DIVERSALIS__COMPILER__MICROSOFT
 		#pragma message("packageneric::pre_compiled:: parsing standard posix headers")
 	#endif
+
 	#include <sys/unistd.h>
+
 	#if defined DIVERSALIS__COMPILER__MICROSOFT
 		#pragma message("packageneric::pre_compiled:: done parsing standard posix headers")
 	#endif
 #endif
 
-
-
-//////////////////////////
+/******************************************************************************************************************/
 // boost http://boost.org
-//////////////////////////
 
-#if defined DIVERSALIS__COMPILER__MICROSOFT
-	#pragma message("packageneric::pre_compiled:: parsing boost headers")
-#endif
-#include <boost/version.hpp>
-#include <boost/static_assert.hpp>
-//#include "boost/multi_array.hpp"
-#include <boost/filesystem/path.hpp>
-#define BOOST_THREAD_USE_DLL
-#include <boost/thread/thread.hpp>
-#include <boost/thread/mutex.hpp>
-#include <boost/thread/recursive_mutex.hpp>
-#if BOOST_VERSION == 103200
-	#include <boost/thread/read_write_mutex.hpp>
-#endif
-#include <boost/thread/condition.hpp>
-// huge include! #include <boost/spirit.hpp>
-#if BOOST_VERSION >= 103301
-//	#include <boost/archive/text_iarchive.hpp>
-//	#include <boost/archive/text_oarchive.hpp>
-	#include <boost/archive/xml_iarchive.hpp>
-	#include <boost/archive/xml_oarchive.hpp>
-	#if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
-		#pragma warning(push)
-		#pragma warning(disable:4267) // 'argument' : conversion from 'size_t' to 'std::streamsize', possible loss of data
-	#endif
-//	#include <boost/archive/binary_iarchive.hpp>
-//	#include <boost/archive/binary_oarchive.hpp>
-	#if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION >= 1400
-		#pragma warning(pop)
-	#endif
-//	#include <boost/serialization/level.hpp>
-//	#include <boost/serialization/version.hpp>
-//	#include <boost/serialization/tracking.hpp>
-//	#include <boost/serialization/export.hpp>
-	#include <boost/serialization/nvp.hpp>
-//	#include <boost/serialization/list.hpp>
-	#include <boost/serialization/string.hpp>
-#endif
-#if defined DIVERSALIS__COMPILER__MICROSOFT
-	#pragma message("packageneric::pre_compiled:: done parsing boost headers")
-#endif
+#include "boost.private.hpp"
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+/******************************************************************************************************************/
 
 #if defined DIVERSALIS__COMPILER__MICROSOFT && DIVERSALIS__COMPILER__VERSION < 1300
 	#pragma warning(pop)
 #endif
 
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
 #if defined DIVERSALIS__COMPILER__MICROSOFT
 	#pragma message("packageneric::pre_compiled:: done parsing " __FILE__)
 #endif
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// end
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 #endif // defined DIVERSALIS__COMPILER__FEATURE__PRE_COMPILATION
 #endif // !defined PACKAGENERIC__PRE_COMPILED__INCLUDED
