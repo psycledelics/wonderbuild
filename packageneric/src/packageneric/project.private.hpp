d3a0afe71e0997d2d56646f690960a53d68a6390
Johan Boule <johan.boule@retropaganda.info>
2008-10-30 19:38:11 +0000

    emacs mode line
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@7355 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- packageneric/src/packageneric/project.private.hpp
+++ packageneric/src/packageneric/project.private.hpp
@@ -1,4 +1,3 @@
-// -*- mode:c++; indent-tabs-mode:t -*-
 // This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 // copyright 1999-2008 members of the psycle project http://psycle.pastnotecut.org ; johan boule <bohan@jabber.org>
 
