e1934101d370b827a78647e36772ae1234f4ae1b
Johan Boule <johan.boule@retropaganda.info>
2008-11-14 14:04:20 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@7471 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- tests/wonderbuild/filesystem.py
+++ tests/wonderbuild/filesystem.py
@@ -2,136 +2,166 @@
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2008-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
-SAVED_ATTRS = 'root srcnode bldnode node_sigs node_deps raw_deps task_sigs id_nodes'.split()
-"Build class members to save"
+import os, os.path, stat
 
-class BuildContext(object):
-	"holds the dependency tree"
+class Tree(object):
 	def __init__(self):
+		self.node_id = 0
+		self.nodes = {} # {id: node}
+		self.node_abspath = {} # {id: abspath }
+		self.dir_lists = {} # {id: [names] }
+		self.cache_path = '/tmp/filesystem.cache'
 
-		# there should be only one build dir in use at a time
-		global bld
-		bld = self
-
-		self.task_manager = Task.TaskManager()
-
-		# instead of hashing the nodes, we assign them a unique id when they are created
-		self.id_nodes = 0
-
-		# map names to environments, the 'default' must be defined
-		self.all_envs = {}
-
-		# ======================================= #
-		# code for reading the scripts
-
-		# project build directory - do not reset() from load_dirs()
-		self.bdir = ''
-
-		# the current directory from which the code is run
-		# the folder changes everytime a wscript is read
-		self.path = None
-
-		# ======================================= #
-		# cache variables
-
-		# local cache for absolute paths - cache_node_abspath[variant][node]
-		self.cache_node_abspath = {}
-
-		# list of folders that are already scanned
-		# so that we do not need to stat them one more time
-		self.cache_scanned_folders = {}
-
-		# list of targets to uninstall for removing the empty folders after uninstalling
-		self.uninstall = []
-
-		# ======================================= #
-		# tasks and objects
-
-		# build dir variants (release, debug, ..)
-		for v in 'cache_node_abspath task_sigs node_deps raw_deps node_sigs'.split():
-			var = {}
-			setattr(self, v, var)
-
-		self.cache_dir_contents = {}
-
-		self.all_task_gen = []
-		self.task_gen_cache_names = {}
-		self.cache_sig_vars = {}
-		self.log = None
-
-		self.root = None
-		self.srcnode = None
-		self.bldnode = None
-
-		# now your head will explode .. :-)
-		class node_class(Node.Node):
-			pass
-		self.node_class = node_class
-		self.node_class.__module__ = "Node"
-		self.node_class.__name__ = "Nodu"
-		self.node_class.bld = self
+	load_save = ('nodes')
 
 	def load(self):
-		"load the cache from the disk"
+		gc.disable()
 		try:
-			env = Environment.Environment(os.path.join(self.cachedir, 'build.config.py'))
-		except (IOError, OSError):
-			pass
-		else:
-			if env['version'] < HEXVERSION:
-				raise Utils.WafError('Version mismatch! reconfigure the project')
-			for t in env['tools']:
-				self.setup(**t)
-
-		try:
-			gc.disable()
-			f = data = None
-
-			Node.Nodu = self.node_class
-
 			try:
-				f = open(os.path.join(self.bdir, DBFILE), 'rb')
-			except (IOError, EOFError):
-				# handle missing file/empty file
-				pass
-
-			try:
-				if f: data = cPickle.load(f)
-			except AttributeError:
-				# handle file of an old Waf version
-				# that has an attribute which no longer exist
-				# (e.g. AttributeError: 'module' object has no attribute 'BuildDTO')
-				if Logs.verbose > 1: raise
-
-			if data:
-				for x in SAVED_ATTRS: setattr(self, x, data[x])
+				f = file(self.cache_path)
+				try: data = cPickle.load(f)
+				finally: f.close()
+			except:
+				self.nodes = {}
 			else:
-				debug('build: Build cache loading failed')
-
-		finally:
-			if f: f.close()
-			gc.enable()
+				for x in load_save: setattr(self, x, data[x])
+		finally: gc.enable()
 
 	def save(self):
-		"store the cache on disk, see self.load"
 		gc.disable()
-		self.root.__class__.bld = None
-
-		Node.Nodu = self.node_class
-		file = open(os.path.join(self.bdir, DBFILE), 'wb')
-		data = {}
-		for x in SAVED_ATTRS: data[x] = getattr(self, x)
-		cPickle.dump(data, file, -1) # remove the '-1' for unoptimized version
-		file.close()
-		self.root.__class__.bld = self
-		gc.enable()
-
-class FSNode(object):
-	__slots__ = ('parent', 'name', 'sig', 'deps')
-
-class FileSystem(object):
-	def __init__(self):
-		self._nodes = {}
-
-	nodes = property(self._nodes)
+		try:
+			f = file(self.cache_path)
+			try:
+				data = {}
+				for x in load_save: data[x] = getattr(self, x)
+				cPicle.dump(data, f, cPickle.HIGHEST_PROTOCOL)
+			finally: f.close()
+		finally: gc.enable()
 	
+	def dump(self):
+		for e in self.nodes.itervalues(): e.dump()
+
+UNKNOWN = 0
+DIR = 1
+FILE = 2
+
+class Entry(object):
+	kind = UNKNOWN
+	
+	__slots__ = ('id', 'parent', 'name', 'time')
+
+	id_counter = 0
+
+	def __init__(self, parent, name, time = None):
+		self.id = self.__class__.id_counter
+		self.__class__.id_counter += 1
+		self.parent = parent
+		self.name = name
+		self.time = time
+		
+	def abs_path(self): return not self.parent and self.name or os.path.join(self.parent.abs_path, self.name)
+	abs_path = property(abs_path)
+	
+	def do_stat(self): return os.stat(self.abs_path())
+	
+	def maybe_stat(self):
+		if self.kind != UNKNOWN: return self
+		return self.do_stat()
+
+	def do_stat(self):
+		st = os.stat(self.abs_path)
+		Kind = stat.S_ISDIR(st.st_mode) and Dir or File
+		e = Kind(self.parent, self.name, st.st_mtime)
+		self.parent.children[self.name] = e
+		return e
+	
+	def dump(self, tabs = 0):
+		print \
+			str(self.id).rjust(4), \
+			(self.time is None and ' ' or str(self.time)).rjust(13), \
+			{UNKNOWN: '', DIR: 'dir', FILE: 'file'}[self.kind].ljust(7) + \
+			'\t' * tabs, self.parent and self.name  or self.abs_path
+
+class Dir(Entry):
+	kind = DIR
+	
+	__slots__ = ('children')
+
+	def __init__(self, parent, name, time = None):
+		Entry.__init__(self, parent, name, time)
+		self.children = None
+	
+	def find(self, path, start = 0):
+		sep = path.find(os.sep, start)
+		if sep < 0:
+			p = path[start:]
+			if p == os.pardir: return self.parent or self
+			if p == os.curdir: return self
+			self.maybe_list_children()
+			return self.children.get(p, None)
+		p = path[start:sep]
+		if p == os.pardir: return self.parent or self
+		if p == os.curdir: return self
+		self.maybe_list_children()
+		child = self.children.get(p, None)
+		if child is None: return None
+		if sep == len(path) - 1: return child
+		child = child.maybe_stat()
+		if child.kind == FILE: return None
+		return child.find_child(path, sep + 1)
+	
+	def maybe_list_children(self):
+		if self.children is None: self.do_list_children()
+	
+	def do_list_children(self):
+		path = self.abs_path
+		self.children = {}
+		for name in os.listdir(path): self.children[name] = Entry(self, name)
+
+	def maybe_list_stat_children(self):
+		self.maybe_list_children()
+		for e in self.children.itervalues(): e.maybe_stat()
+	
+	def do_list_stat_children(self):
+		path = self.abs_path
+		self.children = {}
+		for name in os.listdir(path):
+			st = os.stat(os.path.join(path, name))
+			Kind = stat.S_ISDIR(st.st_mode) and Dir or Kind
+			self.children[name] = Kind(self, name, st.st_mtime)
+
+	def do_stat(self):
+		st = Entry.do_stat(self)
+		if not stat.S_ISDIR(st): raise IOError, 'not a dir'
+		return st
+
+	def update_sig(self, sig):
+		FSNode.update_sig(self, sig)
+		for n in self.children(): n.update_sig(sig)
+
+	def dump(self, tabs = 0):
+		Entry.dump(self, tabs)
+		if not self.children: return
+		tabs += 1
+		for e in self.children.itervalues(): e.dump(tabs)
+
+class File(Entry):
+	kind = FILE
+
+	def do_stat(self):
+		st = Entry.do_stat(self)
+		if stat.S_ISDIR(st): raise IOError, 'not a file'
+		return st
+
+	def update_sig(self, sig):
+		FSNode.update_sig(self, sig)
+		stat_sig(sig, self.stat())
+
+if __name__ == '__main__':
+	root = Dir(None, os.curdir)
+	e = root.find('waf-test').maybe_stat()
+	e = root.find('unit_tests').maybe_stat()
+	e.maybe_list_stat_children()
+	tree = Tree()
+	tree.nodes[root.name] = root
+	tree.dump()
--- tests/wonderbuild/node.py
+++ tests/wonderbuild/node.py
@@ -7,8 +7,8 @@ from import signature import Signed, stat_sig
 
 class Node(Signed):
 	def __init__(self, name):
-		self._name = name
-		self._dep_nodes = []
+		self.name = name
+		self.dep_nodes = []
 		
 	def name(self): return self._name
 
@@ -20,70 +20,14 @@ class Node(Signed):
 class AliasNode(Node):
 	def __init__(self, name, nodes):
 		Node.__init__(self, name)
-		self._dep_nodes = nodes
+		self.dep_nodes = nodes
 
 class ValueNode(Node):
 	def __init__(self, parent_node, value):
 		Node.__init__(self, parent_node, name = None)
-		self._value = value
+		self.value = value
 
 	def update_sig(self, sig):
 		Node.update_sig(self, sig)
 		sig.update(self._value)
-
-class FSNode(Node):
-	def __init__(self, parent_node, name):
-		Node.__init__(self, name)
-		self._parent = parent_node
-		
-	def abs_path(self): return os.path.join(self._parent.abs_path(), self.name())
-	
-	def stat(self):
-		try: return self._stat
-		except AttributeError:
-			self._stat = os.stat(self.abs_path())
-			return self._stat
-
-class DirFSNode(FSNode):
-	'A dir is up-to-date if all of its children nodes are up-to-date'
-
-	def children(self):
-		try: return self._children
-		except AttributeError:
-			self.scan_children()
-			return self._children
-
-	def scan_children(self):
-		self._children = []
-		for name in os.listdir(self.abs_path()): self._children.append(FSNode(self, name))
-
-	def stat(self):
-		stat = FSNode.stat(self):
-		if not stat.S_ISDIR(stat): raise IOError, 'not a dir'
-		return stat
-
-	def update_sig(self, sig):
-		FSNode.update_sig(self, sig)
-		for n in self.children(): n.update_sig(sig)
-
-class RootDirFSNode(FSNode):
-	def __init__(self, abs_path):
-		DirFSNode.__init__(self, parent_node = None, name = os.basename(abs_path))
-		self._abs_path = abs_path
-	
-	def abs_path(self): return self._abs_path
-	
-class FileFSNode(FSNode):
-	def stat(self):
-		stat = FSNode.stat(self):
-		if stat.S_ISDIR(stat): raise IOError, 'not a file'
-		return stat
-
-	def update_sig(self, sig):
-		FSNode.update_sig(self, sig)
-		stat_sig(sig, self.stat())
-
-class FSTree:
-	def __init__(self):
-		self._nodes = []
 
