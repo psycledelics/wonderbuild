ad001b805fb868cb0c94dafec40e172528a01844
Johan Boule <johan.boule@retropaganda.info>
2008-11-23 10:19:01 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@7514 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- tests/wonderbuild/task.py
+++ tests/wonderbuild/task.py
@@ -13,13 +13,12 @@ class Signed(object):
 	sig = property(get_sig)
 
 class Task(Signed):
-	def __init__(self, in_nodes, out_nodes):
-		self.in_nodes = in_nodes
-		self.out_nodes = out_nodes
+	def __init__(self):
+		self.in_tasks = []
+		self.in_nodes = []
+		self.out_nodes = []
 		self.processed = False
 
-	def dyn_deps(self): pass
-			
 	def process(self): pass
 
 	def get_sig(self):
@@ -27,23 +26,11 @@ class Task(Signed):
 		for n in self.in_nodes: sig.update(n.sig)
 		return sig
 		
-class FuncTask(Task):
-	def __init__(self, in_nodes, func, out_nodes):
-		Task.__init__(self, in_nodes, out_nodes)
-		self.func = func
-		
-	def process(self): self.func()
-
-	def get_sig(self):
-		sig = Task.get_sig(self)
-		sig.update(self.func.code)
-		return sig
-		
 class CmdTask(Task):
-	def __init__(self, in_nodes, env, cmd_args, out_nodes):
-		Task.__init__(self, in_nodes, out_nodes)
-		self.env = env
-		self.cmd_args = cmd_args
+	def __init__(self):
+		Task.__init__(self)
+		self.env = None
+		self.cmd_args = None
 		
 	def process(self):
 		r = exec_subprocess(self.env, self.cmd_args)
@@ -53,6 +40,31 @@ class CmdTask(Task):
 		sig = Task.get_sig(self)
 		for k, v in self.env:
 			sig.update(k)
+			sig.update('\0')
 			sig.update(v)
+			sig.update('\0')
 		sig.update(self.cmd_args)
 		return sig
+
+class ObjTask(Cmd):
+	def __init__(self):
+		Cmd.__init__(self, cxx)
+		self.in_nodes = [cxx]
+		self.out_nodes = [cxx.change_ext('cpp', 'o')]
+		self.cmd_args = ['c++', '-o', self.out_nodes[0], '-c', self.in_nodes[0]]
+		
+class LibTask(Cmd):
+	def __init__(self, objs):
+		Cmd.__init__(self)
+		self.in_nodes = objs
+
+	def dyn_deps(self):
+		for o in self.in_nodes:
+			self.
+	
+	def process(self):
+		self.cmd_args = ['c++', '-o', self.out_nodes[0], '-c', self.in_nodes[0]]
+		self.in_nodes.append(n)
+			t = ObjTask()
+			t.in_nodes = [n]
+			t.out_nodes = [
