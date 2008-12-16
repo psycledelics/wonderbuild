66240bdeb1ef31eee8353b096475a2ffeba9f18f
Johan Boule <johan.boule@retropaganda.info>
2008-12-16 19:43:24 +0000

    main
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@7681 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- wonderbuild/main.py
+++ wonderbuild/main.py
@@ -2,20 +2,27 @@
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2008-2008 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
+import sys, os
+	
 def run():
 	from project import Project
 	project = Project()
 
 	d = {}
-	execfile('wonderbuild_script.py', d)
-	tasks = d['wonderbuild_script'](project)
+	if os.path.exists('wonderbuild_script.py'):
+		execfile('wonderbuild_script.py', d)
+		tasks = d['wonderbuild_script'](project)
+		usage = False
+	else:
+		print >> sys.stderr, 'no wonderbuild_script.py found'
+		usage = True
 
 	from options import options, known_options, help
 	help['--version'] = ('--version', 'show the version of this tool and exit')
 
-	if '--help' in options:
+	def print_help(out):
 		help['--help'] = ('--help', 'show this help and exit')
-		
+
 		project.help()
 		keys = []
 		just = 0
@@ -27,23 +34,33 @@ def run():
 		for h in keys:
 			h = help[h]
 			print h[0].ljust(just), h[1]
-			if len(h) >= 3: print ''.ljust(just), '(default: ' + h[2] + ')'
-		return
+			if len(h) >= 3: print >> out, ''.ljust(just), '(default: ' + h[2] + ')'
+
+	if '--help' in options:
+		print_help(sys.stdout)
+		return 0
 
 	if '--version' in options:
 		print 'wonderbuild 0.1'
-		return
-
+		return 0
+	
 	project.options()
 	for o in options:
 		if o.startswith('-'):
 			e = o.find('=')
 			if e >= 0: o = o[:e]
-			if o not in known_options: raise Exception, 'unknown option: ' + o
+			if o not in known_options:
+				print >> sys.stderr, 'unknown option: ' + o
+				usage = True
+
+	if usage:
+		print_help(sys.stderr)
+		return 1
 
 	project.conf()
 	project.build(tasks)
 	project.dump()
+	return 0
 
 def main():
 	#import gc; gc.disable()
@@ -54,9 +71,9 @@ def main():
 
 	if '--profile' in options:
 		import cProfile, pstats
-		cProfile.run('run()', '/tmp/profile')
+		cProfile.run('sys.exit(run())', '/tmp/profile')
 		p = pstats.Stats('/tmp/profile')
 		p.sort_stats('cumulative').reverse_order().print_stats()
-	else: run()
+	else: sys.exit(run())
 
 if __name__ == '__main__': main()
