8a58ed341fa3e1d26b708733b42b8346df968afc
Johan Boule <johan.boule@retropaganda.info>
2009-01-11 16:56:10 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@7869 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- wonderbuild/benchmarks/misc/stat/stat.cpp
+++ wonderbuild/benchmarks/misc/stat/stat.cpp
@@ -1,16 +1,21 @@
 #include <sys/stat.h>
 #include <iostream>
+#include <sstream>
 #include <errno.h>
 
 int main() {
 	time_t max_time;
 	struct stat st;
-	for(unsigned int i(0); i < 1000000; ++i) {
-		if(stat(".", &st) != 0) {
-			std::cerr << errno << '\n';
-			return errno;
-		}
-		if(st.st_mtime > max_time) max_time = st.st_mtime;
+	for(unsigned int i(0); i < 200; ++i)
+		for(unsigned int lib(0); lib < 50; ++lib)
+			for(unsigned int cpp(0); cpp < 100; ++cpp) {
+				std::ostringstream s;
+				s << "../../bench/lib_" << lib << "/class_" << cpp << ".cpp";
+				if(stat(s.str().c_str(), &st) != 0) {
+					std::cerr << errno << '\n';
+					return errno;
+				}
+				if(st.st_mtime > max_time) max_time = st.st_mtime;
 	}
 	std::cout << max_time << '\n';
 	return 0;
--- wonderbuild/benchmarks/misc/stat/stat.py
+++ wonderbuild/benchmarks/misc/stat/stat.py
@@ -3,9 +3,13 @@
 if __name__ == '__main__':
 	import sys, os, stat, time
 	max_time = time.time()
-	for i in xrange(1000000):
-		try: st = os.stat('.')
-		except OSError: sys.exit(1)
-		t = st.st_mtime
-		if t > max_time: max_time = t
-	print max_time		
+	
+	for i in xrange(200):
+		for lib in xrange(50):
+			for cpp in xrange(100):
+				path = '../../bench/lib_' + str(lib) + '/class_' + str(cpp) + '.cpp'
+				try: st = os.stat(path)
+				except OSError: sys.exit(1)
+				t = st.st_mtime
+				if t > max_time: max_time = t
+	print max_time
