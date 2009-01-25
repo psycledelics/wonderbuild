3594eb466fc8231801596c2ad0e6b8fdd951a632
Johan Boule <johan.boule@retropaganda.info>
2009-01-25 16:25:34 +0000

    subprocess stress test
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@7919 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ wonderbuild/benchmarks/misc/subprocess_stress_test.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+if __name__ == '__main__':
+
+	import os, threading, time
+	
+	#import pproc as subprocess
+	import subprocess
+	
+	thread_count = 2
+	input = 'int main() { return 0; }\n'
+	args = ['c++', '-xc++', '-', '-o', os.devnull]
+
+	def thread_function(input, args):
+		while True:
+			p = subprocess.Popen(
+				args = args,
+				bufsize = -1,
+				stdin = input and subprocess.PIPE,
+				stdout = subprocess.PIPE,
+				stderr = subprocess.PIPE,
+			)
+			out, err = p.communicate(input)
+			print p.returncode, time.time()
+
+	threads = []
+	for i in xrange(thread_count):
+		t = threading.Thread(target = thread_function, args = (input, args), name = 'thread-' + str(i))
+		#t.daemon = True
+		t.setDaemon(True)
+		t.start()
+		threads.append(t)
+		
+	for t in threads: t.join(timeout = 3600.0)
