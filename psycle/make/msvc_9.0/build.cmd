8009b59792c402211d9fa4297791a62cabe2a3f6
Johan Boule <johan.boule@retropaganda.info>
2009-04-04 19:20:38 +0000

    centralised platform options in property sheets
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8115 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle/make/msvc_9.0/build.cmd
+++ psycle/make/msvc_9.0/build.cmd
@@ -4,7 +4,7 @@ set configuration=%1
 if "%configuration%" == "" (
 	set configuration=release
 )
-call "%VS80ComnTools%\VSVars32" || exit /b 1
+call "%VS90ComnTools%\VSVars32" || exit /b 1
 rem msbuild solution.sln -property:configuration=%configuration% || exit /b 1
 rem vcbuild solution.sln "%configuration%|Win32" || exit /b 1
 DevEnv solution.sln /useenv /build %configuration% || exit /b 1
