cc81cd1fec6bff6c52adaa7a112e32e2e978d4af
Johan Boule <johan.boule@retropaganda.info>
2009-04-12 00:25:47 +0000

    wonderbuild script for diversalis
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8172 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ diversalis/wonderbuild_script.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+def wonderbuild_script(project):
+
+	from wonderbuild.install import InstallTask
+	from wonderbuild.fhs_cfg import FHSCfg
+	
+	tasks = []
+
+	src_dir = project.src_node / 'src'
+
+	fhs_cfg = FHSCfg(project)
+
+	class Diversalis(InstallTask):
+		def __init__(self): InstallTask.__init__(self, project)
+
+		@property
+		def trim_prefix(self): return src_dir
+
+		@property
+		def sources(self):
+			try: return self._sources
+			except AttributeError:
+				self._sources = []
+				for s in (self.trim_prefix / 'diversalis').find_iter(in_pats = ['*.hpp'], ex_pats = ['*.private.hpp'], prune_pats = ['todo']): self._sources.append(s)
+				return self._sources
+	
+		@property
+		def dest_dir(self): return fhs_cfg.include
+	diversalis = Diversalis()
+	
+	tasks.append(diversalis)
+
+	return tasks
