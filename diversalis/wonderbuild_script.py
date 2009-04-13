f3102d246327341a7904a140db332a2104c0c27f
Johan Boule <johan.boule@retropaganda.info>
2009-04-13 20:37:12 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8186 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- diversalis/wonderbuild_script.py
+++ diversalis/wonderbuild_script.py
@@ -2,27 +2,31 @@
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
-def wonderbuild_script(project, src_dir):
+from wonderbuild.script import ScriptTask
 
-	src_dir = src_dir / 'src'
+class Wonderbuild(ScriptTask):
+	def __call__(self, sched_ctx):
 
-	from wonderbuild.install import InstallTask
+		src_dir = self.src_dir / 'src'
+		project = self.project
+
+		from wonderbuild.install import InstallTask
 	
-	class Diversalis(InstallTask):
-		def __init__(self): InstallTask.__init__(self, project)
+		class Diversalis(InstallTask):
+			def __init__(self): InstallTask.__init__(self, project)
 
-		@property
-		def trim_prefix(self): return src_dir
+			@property
+			def trim_prefix(self): return src_dir
 
-		@property
-		def sources(self):
-			try: return self._sources
-			except AttributeError:
-				self._sources = []
-				for s in (self.trim_prefix / 'diversalis').find_iter(in_pats = ['*.hpp'], ex_pats = ['*.private.hpp'], prune_pats = ['todo']): self._sources.append(s)
-				return self._sources
+			@property
+			def sources(self):
+				try: return self._sources
+				except AttributeError:
+					self._sources = []
+					for s in (self.trim_prefix / 'diversalis').find_iter(in_pats = ['*.hpp'], ex_pats = ['*.private.hpp'], prune_pats = ['todo']): self._sources.append(s)
+					return self._sources
 	
-		@property
-		def dest_dir(self): return self.fhs.include
-	
-	return [Diversalis()]
+			@property
+			def dest_dir(self): return self.fhs.include
+		
+		self.project.add_task_aliases(Diversalis(), 'all')
--- universalis/wonderbuild_script.py
+++ universalis/wonderbuild_script.py
@@ -2,35 +2,37 @@
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
-def wonderbuild_script(project, src_dir):
+from wonderbuild.script import ScriptTask
 
-	from wonderbuild.script import Script
-	diversalis = Script(project, src_dir.parent / 'diversalis')()
+class Wonderbuild(ScriptTask):
+	def __call__(self, sched_ctx):
 
-	src_dir = src_dir / 'src'
+		diversalis = self.project.script_task(self.src_dir.parent / 'diversalis')
 
-	from wonderbuild.install import InstallTask
+		project = self.project
+		src_dir = self.src_dir / 'src'
+
+		from wonderbuild.install import InstallTask
 	
-	class Universalis(InstallTask):
-		def __init__(self): InstallTask.__init__(self, project)
+		class Universalis(InstallTask):
+			def __init__(self): InstallTask.__init__(self, project)
 
-		@property
-		def trim_prefix(self): return src_dir
+			@property
+			def trim_prefix(self): return src_dir
 
-		@property
-		def sources(self):
-			try: return self._sources
-			except AttributeError:
-				self._sources = []
-				for s in (self.trim_prefix / 'universalis').find_iter(in_pats = ['*.hpp'], ex_pats = ['*.private.hpp'], prune_pats = ['todo']): self._sources.append(s)
-				return self._sources
+			@property
+			def sources(self):
+				try: return self._sources
+				except AttributeError:
+					self._sources = []
+					for s in (self.trim_prefix / 'universalis').find_iter(in_pats = ['*.hpp'], ex_pats = ['*.private.hpp'], prune_pats = ['todo']): self._sources.append(s)
+					return self._sources
 	
-		@property
-		def dest_dir(self): return self.fhs.include
+			@property
+			def dest_dir(self): return self.fhs.include
 		
-		def __call__(self, sched_ctx):
-			sched_ctx.parallel_wait(diversalis)
-			InstallTask.__call__(self, sched_ctx)
-	universalis = Universalis()
-
-	return [universalis] + diversalis
+			def __call__(self, sched_ctx):
+				sched_ctx.parallel_wait(diversalis)
+				InstallTask.__call__(self, sched_ctx)
+		
+		self.project.add_task_aliases(Universalis(), 'all')
