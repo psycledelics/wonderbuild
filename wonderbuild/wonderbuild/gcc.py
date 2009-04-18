d1a437f49010ed71608287bac04d3f880e4c9534
Johan Boule <johan.boule@retropaganda.info>
2009-04-18 21:56:01 +0000

    target platform
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@8206 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- wonderbuild/wonderbuild/gcc.py
+++ wonderbuild/wonderbuild/gcc.py
@@ -196,7 +196,7 @@ class Impl(object):
 	def mod_task_target_name(mod_task):
 		if mod_task.kind == mod_task.Kinds.PROG: name = mod_task.name
 		elif mod_task.cfg.shared:
-			if mod_task.target_platform.startswith('win'): name = mod_task.name + '.dll'
+			if mod_task.cfg.target_platform.startswith('win'): name = mod_task.name + '.dll'
 			else: name = 'lib' + mod_task.name + '.so'
 		else: name = 'lib' + mod_task.name + '.a'
 		return name
