7128626b3634738109c8c9c649e6e2c89ad2722f
Johan Boule <johan.boule@retropaganda.info>
2009-05-17 17:36:10 +0000

    big reorg
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8362 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/msvc/build-solution.msvc-2008.cmd
+++ build-systems/msvc/build-solution.msvc-2008.cmd
@@ -5,6 +5,6 @@ if "%configuration%" == "" (
 	set configuration=release
 )
 call "%VS90ComnTools%\VSVars32" || exit /b 1
-rem msbuild solution.sln -property:configuration=%configuration% || exit /b 1
-rem vcbuild solution.sln "%configuration%|Win32" || exit /b 1
-DevEnv solution.sln /useenv /build %configuration% || exit /b 1
+rem msbuild solution.msvc-2008.sln -property:configuration=%configuration% || exit /b 1
+rem vcbuild solution.msvc-2008.sln "%configuration%|Win32" || exit /b 1
+DevEnv solution.msvc-2008.sln /useenv /build %configuration% || exit /b 1
