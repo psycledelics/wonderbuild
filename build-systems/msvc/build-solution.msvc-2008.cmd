926a05d6b9d02a099319dc3afa2b8b4d769b96da
Johan Boule <johan.boule@retropaganda.info>
2009-05-21 09:28:29 +0000

    vcbuild is faster
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8423 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/msvc/build-solution.msvc-2008.cmd
+++ build-systems/msvc/build-solution.msvc-2008.cmd
@@ -6,5 +6,5 @@ if "%configuration%" == "" (
 )
 call "%VS90ComnTools%\VSVars32" || exit /b 1
 rem msbuild solution.msvc-2008.sln -property:configuration=%configuration% || exit /b 1
-rem vcbuild solution.msvc-2008.sln "%configuration%|Win32" || exit /b 1
-DevEnv solution.msvc-2008.sln /useenv /build %configuration% || exit /b 1
+vcbuild solution.msvc-2008.sln "%configuration%|Win32" || exit /b 1
+rem DevEnv solution.msvc-2008.sln /useenv /build %configuration% || exit /b 1
