af1b8470f5e301cdde0221771cff3acf8a849bf5
Johan Boule <johan.boule@retropaganda.info>
2009-05-26 11:05:28 +0000

    made the wonderbuild_script files executable
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8498 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ freepsycle/wonderbuild_script.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+if __name__ == '__main__':
+	try: from wonderbuild.main import main
+	except ImportError:
+		import sys, os
+		dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'build-systems'))
+		if dir not in sys.path: sys.path.append(dir)
+		try: from wonderbuild.main import main
+		except ImportError:
+			print >> sys.stderr, 'could not find wonderbuild'
+			sys.exit(1)
+		else: main()
+	else: main()
+
+from wonderbuild.script import ScriptTask
+
+class Wonderbuild(ScriptTask):
+	def __call__(self, sched_ctx):
+		project = self.project
+		top_src_dir = self.src_dir.parent
+		src_dir = self.src_dir / 'src'
+
