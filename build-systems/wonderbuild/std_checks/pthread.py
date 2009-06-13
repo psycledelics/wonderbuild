f4e677a50dc694926182d88301cd07330250c749
Johan Boule <johan.boule@retropaganda.info>
2009-06-13 10:41:57 +0000

    pthread
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8585 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/std_checks/pthread.py
+++ build-systems/wonderbuild/std_checks/pthread.py
@@ -8,7 +8,10 @@ class PThreadCheckTask(BuildCheckTask):
 	def __init__(self, base_cfg): BuildCheckTask.__init__(self, 'posix-thread', base_cfg)
 
 	def apply_to(self, cfg):
-		cfg.libs.append('pthread')
+		if cfg.kind == 'gcc':
+			cfg.cxx_flags.append('-pthread')
+			cfg.ld_flags.append('-pthread')
+		else: cfg.libs.append('pthread')
 
 	@property
 	def source_text(self): return \
