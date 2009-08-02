6026f452e182d383d32c6f516118ef65cd5dca6d
Johan Boule <johan.boule@retropaganda.info>
2009-08-02 18:55:21 +0000

    fixed the grey ramp
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8769 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/logger.py
+++ build-systems/wonderbuild/logger.py
@@ -109,21 +109,36 @@ else:
 		_map_b = []
 		_map_grey = []
 		def _fill_maps():
-			# note: xterm and mrxvt support changing the palette: http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
-			#       gnome-terminal has a fixed palette.
 			scale = {88: 4, 256: 6}[colors]
-			scale_grey = {88: 8, 256: 24}[colors]
-			offset_grey = {88: 80, 256: 232}[colors]
 			for v in xrange(256):
-				_map_grey.append(str(offset_grey + v * scale_grey // 256))
-				# TODO there are also grey colors in the color cube (4 in 256-color mode, not counting black and white)
 				v = v * scale // 256
 				_map_b.append(v)
 				v *= scale
 				_map_g.append(v)
 				_map_r.append(v * scale)
-			_map_grey[0] = str(16)
-			_map_grey[255] = str(offset_grey - 1)
+			if colors == 88:
+				i = 0; c1 = 0x2e
+				for v in xrange(0, c1): _map_grey.append('16')
+				for c2 in (0x5c, 0x73, 0x8b, 0xa2, 0xb9, 0xd0, 0xe7):
+					for v in xrange(c1, c2): _map_grey.append(str(80 + i))
+					i += 1; c1 = c2
+				for v in xrange(c2, 256): _map_grey.append('87')
+				# there are also 2 grey colors in the 4x4x4 color cube, not counting black and white
+				#for x in xrange(0x?? - ?, 0x?? + ?): _map_grey[x] = '?'
+				#for x in xrange(0x?? - ?, 0x?? + ?): _map_grey[x] = '?'
+			elif colors == 256:
+				i = 0; c0 = c1 = 8
+				for v in xrange(0, c1): _map_grey.append('16')
+				for c2 in (c0 + x for x in xrange(0, 240, 10)):
+					for v in xrange(c1, c2): _map_grey.append(str(231 + i))
+					i += 1; c1 = c2
+				for v in xrange(c2, 256): _map_grey.append('255')
+				if True:
+					# there are also 4 grey colors in the 6x6x6 color cube, not counting black and white
+					for x in xrange(0x5f, 0x5f + 5): _map_grey[x] = '59'
+					for x in xrange(0x87, 0x87 + 5): _map_grey[x] = '102'
+					for x in xrange(0xaf, 0xaf + 5): _map_grey[x] = '145'
+					for x in xrange(0xd7, 0xd7 + 5): _map_grey[x] = '188'
 		_fill_maps()
 		def _merge_rgb(rgb):
 			if rgb[0] == rgb[1] and rgb[1] == rgb[2]: return _map_grey[rgb[0]]
@@ -134,46 +149,61 @@ else:
 		def color_bg_fg_rgb(bg, fg): return ''
 
 if __name__ == '__main__':
+	# 8 colors (3-bit: bgr with 1 on bit per channel)
 	for i in xrange(8):
 		bg = '4' + str(i)
-		c = bg + ';3' + str(7 - i)
-		s = '%02d' % i + ' '
-		out.write(colored(c, s))
+		fg = 7 - i
+		c = bg + ';3' + str(fg)
+		out.write(colored(c, '%02d/%02d' % (fg, i)))
 		for x in ('1', '2', '5', '6'): out.write(colored(c + ';' + x, ' ' + x + ' '))
-		out.write(colored(bg + ';37', 'wh '))
-		out.write(colored(bg + ';30', 'bl '))
+		out.write(colored(bg + ';37', '07'))
+		out.write(colored(bg + ';30', '00'))
 		out.write(' ')
 	out.write('\n')
+	# 16 colors (4-bit: bgr with 1 on bit per channel, and 1 bit for highlight)
 	for i in xrange(16):
 		bg = '48;5;' + str(i)
-		c = bg + ';38;5;' + str((i + 8) % 16)
-		s = '%02d' % i + ' '
-		out.write(colored(c, s))
+		fg = (i + 8) % 16
+		c = bg + ';38;5;' + str(fg)
+		out.write(colored(c, '%02d/%02d' % (fg, i)))
 		for x in ('1', '2', '5', '6'): out.write(colored(c + ';' + x, ' ' + x + ' '))
-		out.write(colored(bg + ';38;5;15', 'wh '))
-		out.write(colored(bg + ';38;5;0', 'bl '))
+		out.write(colored(bg + ';38;5;15', '15'))
+		out.write(colored(bg + ';38;5;0', '00'))
 		out.write(' ')
 		if i == 7: out.write('\n')
 	out.write('\n\n')
-	s = [0] + [0x5f + (0x87 - 0x5f) * x for x in xrange(5)]
-	i = 16
-	for r in s:
-		for g in s:
-			for b in s:
-				v = (r + g + b) // 3 < 128 and 255 or 0
-				out.write(colored(color_bg_fg_rgb((r, g, b), (v, v, v)), '%02x/%02x/%02x        ' % (r, g, b)))
-				out.write(colored('48;5;' + str(i) + ';38;5;' + (v != 0 and '231' or '0'), '         %03d' % i) + ' ')
+	if colors == 256:
+		# 6x6x6 color cube (rgb with 6 values per channel)
+		s = [0] + [0x5f + (0x87 - 0x5f) * x for x in xrange(5)]
+		i = 16
+		for r in s:
+			for g in s:
+				for b in s:
+					v = (r + g + b) // 3 < 128 and 255 or 0
+					out.write(colored(color_bg_fg_rgb((r, g, b), (v, v, v)), '%02x/%02x/%02x        ' % (r, g, b)))
+					out.write(colored('48;5;' + str(i) + ';38;5;' + (v != 0 and '231' or '0'), '         %03d' % i) + ' ')
+					i += 1
+				out.write('\n')
+			out.write('\n')
+		# 24-values grey ramp, without black and white
+		for l in xrange(3):
+			i = 232
+			for c in [0x8] + [0x12 + (0x12 - 0x8) * x for x in xrange(23)]:
+				v = c < 128 and 255 or 0
+				out.write(colored(color_bg_fg_rgb((c, c, c), (v, v, v)), l == 0 and '%02x:' % c or '   '))
+				out.write(colored('48;5;' + str(i) + ';38;5;' + (v != 0 and '231' or '0'), l == 0 and '%03d' % i or '   ') + ' ')
 				i += 1
 			out.write('\n')
-		out.write('\n')
-	for l in xrange(3):
-		i = 232
-		for c in [0x8] + [0x12 + (0x12 - 0x8) * x for x in xrange(23)]:
-			v = c < 128 and 255 or 0
-			out.write(colored(color_bg_fg_rgb((c, c, c), (v, v, v)), l == 1 and '%02x ' % c or '   '))
-			out.write(colored('48;5;' + str(i) + ';38;5;' + (v != 0 and '231' or '0'), l == 1 and '%03d' % i or '   ') + ' ')
-			i += 1
-		out.write('\n')
-	if False:
-		for i in xrange(255):
-			out.write('\33]4;' + str(i) + ';?\33\\')
+		for l in xrange(2):
+			for c in [0x8] + [0x12 + (0x12 - 0x8) * x for x in xrange(23)]:
+				for x in (0, 1, 3, 4, 6, 8, 9): out.write(colored(color_bg_fg_rgb((c + x, c + x, c + x), (0, 0, 0)), ' '))
+			out.write('\n')
+		# palette querying (xterm only)
+		# default 256-color palette:
+		# - color cube starts at index 16 with intensity 95 (0x5f) and increments by 40 (0x87 - 0x5f)
+		# - grey ramp starts at index 232 with intensity 8 (0x8) and increments by 10 (0x12 - 0x8)
+		# xterm and mrxvt support changing the palette: http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+		# gnome-terminal has a fixed palette.
+		if False:
+			for i in xrange(255):
+				out.write('\33]4;' + str(i) + ';?\33\\')
