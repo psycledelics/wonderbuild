9b509f3177c3a391272691d82abc11375e0edecc
Johan Boule <johan.boule@retropaganda.info>
2009-08-08 17:59:06 +0000

    tests
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@8786 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ wonderbuild/wonderbuild-test/wonderbuild_script.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2009-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+
+##############################################################################
+#
+# this script simply builds all of the tests
+#
+##############################################################################
+
+
+if __name__ == '__main__':
+	import sys, os
+	dir = os.path.dirname(__file__)
+	sys.argv.append('--src-dir=' + dir)
+	try: from wonderbuild.main import main
+	except ImportError:
+		dir = os.path.abspath(os.path.join(dir, os.pardir))
+		if dir not in sys.path: sys.path.append(dir)
+		try: from wonderbuild.main import main
+		except ImportError:
+			print >> sys.stderr, 'could not find wonderbuild'
+			sys.exit(1)
+		else: main()
+	else: main()
+
+else:
+	from wonderbuild.script import ScriptTask, ScriptLoaderTask
+
+	class Wonderbuild(ScriptTask):
+		def __call__(self, sched_ctx):
+			for x in sched_ctx.parallel_wait(
+				*(
+					ScriptLoaderTask.shared(self.project, self.src_dir.parent / dir) \
+					for dir in (
+						'static-shared',
+						'pre-compiled'
+					)
+				)
+			): yield x
