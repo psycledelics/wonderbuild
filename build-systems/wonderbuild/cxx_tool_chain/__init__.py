2ab49a17f52a0e1a389a33a0f77e76b1c187df95
Johan Boule <johan.boule@retropaganda.info>
2009-08-09 22:23:05 +0000

    that was a very subtle bug
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8810 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/cxx_tool_chain/__init__.py
+++ build-systems/wonderbuild/cxx_tool_chain/__init__.py
@@ -352,21 +352,21 @@ class ModDepPhases(object):
 			self._public_topologically_sorted_unique_deep_deps
 		except AttributeError:
 			result = deque(); seen = set() # ordering matters for sig, and static libs must appear after their clients
-			for dep in self.all_deps: dep._dep_depths(result, seen, True, expose_private_deep_deps)
+			self._dep_depths(result, seen, True, expose_private_deep_deps)
 			if expose_private_deep_deps is None: self._private_cut_topologically_sorted_unique_deep_deps = result
 			elif expose_private_deep_deps: self._private_topologically_sorted_unique_deep_deps = result
 			else: self._public_topologically_sorted_unique_deep_deps = result
 			return result
-		
+	
 	def _dep_depths(self, result, seen, expose_private_deps, expose_private_deep_deps):
-		if self in seen: return
-		seen.add(self)
 		for dep in expose_private_deps and self.all_deps or self.public_deps:
-			if expose_private_deep_deps is None:
-				dep._dep_depths(result, seen, dep.expose_private_deep_deps, dep.expose_private_deep_deps and None)
-			else:
-				dep._dep_depths(result, seen, expose_private_deep_deps, expose_private_deep_deps)
-		result.appendleft(self)
+			if dep not in seen:
+				seen.add(dep)
+				if expose_private_deep_deps is None:
+					dep._dep_depths(result, seen, dep.expose_private_deep_deps, dep.expose_private_deep_deps and None)
+				else:
+					dep._dep_depths(result, seen, expose_private_deep_deps, expose_private_deep_deps)
+				result.appendleft(dep)
 
 class _PreCompileTask(ModDepPhases, ProjectTask):
 	def __init__(self, name, base_cfg):
