1910c3a6fdfbc7ce7c6d5fb649a7d047cad7737e
Johan Boule <johan.boule@retropaganda.info>
2009-08-24 04:49:47 +0000

    parallelism example
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@8860 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- wonderbuild/wonderbuild-test/wonderbuild_script.py
+++ wonderbuild/wonderbuild-test/wonderbuild_script.py
@@ -39,3 +39,48 @@ else:
 					)
 				)
 			): yield x
+			
+			if False:
+				project = self.project
+				from wonderbuild.task import Task
+				class Foo(Task):
+					@staticmethod
+					def shared(hold, x0, x1, y):
+						try: x0s = hold.foos
+						except AttributeError: x0s = hold.foos = {}
+						try: x1s = x0s[x0]
+						except KeyError: x1s = x0s[x0] = {}
+						try: foos = x1s[x1]
+						except KeyError: foos = x1s[x1] = {}
+						try: return foos[y]
+						except KeyError:
+							foo = foos[y] = Foo(x0, x1, y)
+							return foo
+			
+					def __init__(self, x0, x1, y):
+						Task.__init__(self)
+						self.x0 = x0
+						self.x1 = x1
+						self.y = y
+					def __call__(self, sched_ctx):
+						sched_ctx.lock.release() # allow parallelism
+						try:
+							x0, x1, y = self.x0, self.x1, self.y
+							if x1 - x0 > 100:
+								x = (x0 + x1) // 2
+								tasks = Foo.shared(project, x0, x, y), Foo.shared(project, x, x1, y)
+								sched_ctx.lock.acquire() # we need to reacquire the lock before yielding back into the scheduler
+								try:
+									for x in sched_ctx.parallel_wait(*tasks): yield x
+								finally: sched_ctx.lock.release()
+								self.result = tasks[0].result + tasks[1].result
+							else:
+								self.result = []
+								for x in xrange(x0, x1): self.result.append(x * y)
+						finally: sched_ctx.lock.acquire()
+
+				f1 = Foo.shared(project, 0, 2000, 2)
+				f2 = Foo.shared(project, 1000, 3000, 2)
+				for x in sched_ctx.parallel_wait(f1, f2): yield x
+				print f1.result, f2.result
+
