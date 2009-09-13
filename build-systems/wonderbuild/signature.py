baea80dfb798c77e49a8ee744a9509b169cbc81b
Johan Boule <johan.boule@retropaganda.info>
2009-09-13 04:12:03 +0000

    if available, use fnv instead of md5
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8942 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/signature.py
+++ build-systems/wonderbuild/signature.py
@@ -2,8 +2,10 @@
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2008-2009 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
-try: from hashlib import md5 as _Sig
-except ImportError: from md5 import md5 as _Sig
+try: from fnv import new as _Sig
+except ImportError:
+	try: from hashlib import md5 as _Sig
+	except ImportError: from md5 import md5 as _Sig
 
 class Sig(object):
 	def __init__(self, s=None):
