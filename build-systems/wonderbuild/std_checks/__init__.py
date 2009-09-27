be1a5cd3e9c1448f67fa3a25b7ea1e2e1d5270f1
Johan Boule <johan.boule@retropaganda.info>
2009-09-27 22:06:16 +0000

    copying back my code from waf
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@8976 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/std_checks/__init__.py
+++ build-systems/wonderbuild/std_checks/__init__.py
@@ -17,33 +17,133 @@ class BinaryFormatCheckTask(BuildCheckTask):
 	def __init__(self, base_cfg): BuildCheckTask.__init__(self, 'binary-format', base_cfg, pipe_preproc=True)
 	
 	@property
-	def source_text(self): return \
-		'#if defined __ELF__\n' \
-		'\telf\n' \
-		'#elif defined __APPLE__ and defined __MACH__\n' \
-		'\tmac-o\n' \
-		'#elif defined _WIN32 || defined __CYGWIN__ || defined __MSYS__ || defined _UWIN\n' \
-		'\tpe\n' \
-		'#else\n' \
-		'\t#error unkown binary format\n' \
-		'#endif\n'
-	
+	def source_text(self): return '''\
+#if defined __ELF__
+	#define WONDERBUILD__BIN_FMT "elf"
+#elif defined __APPLE__ && defined __MACH__
+	#define WONDERBUILD__BIN_FMT "mac-o"
+#elif defined _WIN32 || defined __CYGWIN__ || defined __MSYS__ || defined _UWIN
+	#define WONDERBUILD__BIN_FMT "pe"
+#else
+	#error unkown binary format
+#endif
+#if defined __linux__
+	#define WONDERBUILD__OS "linux"
+#elif defined __GNU__
+	#define WONDERBUILD__OS "hurd"
+#elif defined __OpenBSD__
+	#define WONDERBUILD__OS "openbsd"
+#elif defined __FreeBSD__
+	#define WONDERBUILD__OS "freebsd"
+#elif defined __NetBSD__
+	#define WONDERBUILD__OS "netbsd"
+#elif defined __sun
+	#define WONDERBUILD__OS "sunos"
+#elif defined __sgi
+	#define WONDERBUILD__OS "irix"
+#elif defined __hpux
+	#define WONDERBUILD__OS "hpux"
+#elif defined _AIX
+	#define WONDERBUILD__OS "aix"
+#elif defined __APPLE__ && defined __MACH__
+	#define WONDERBUILD__OS "darwin"
+#elif defined __CYGWIN__
+	#define WONDERBUILD__OS "cygwin"
+#elif defined __MSYS__
+	#define WONDERBUILD__OS "msys"
+#elif defined _UWIN__
+	#define WONDERBUILD__OS "uwin"
+#elif defined _WIN64 || defined _WIN32
+	#define WONDERBUILD__OS "win"
+#elif defined __unix__ || defined unix
+	#define WONDERBUILD__OS "generic"
+#else
+	#error unkown operating system
+#endif
+#if defined __x86_64__
+	  #define WONDERBUILD__ARCH "x86_64"
+#elif defined __i386__
+	  #define WONDERBUILD__ARCH "x86"
+#elif defined __ia64__
+	  #define WONDERBUILD__ARCH "ia"
+#elif defined __mips__
+	  #define WONDERBUILD__ARCH "mips"
+#elif defined __sparc__
+	  #define WONDERBUILD__ARCH "sparc"
+#elif defined __alpha__
+	  #define WONDERBUILD__ARCH "alpha"
+#elif defined __arm__
+	  #define WONDERBUILD__ARCH "arm"
+#elif defined __hppa__
+	  #define WONDERBUILD__ARCH "hppa"
+#elif defined __powerpc__
+	  #define WONDERBUILD__ARCH "powerpc"
+#endif
+WONDERBUILD__BIN_FMT
+WONDERBUILD__OS
+WONDERBUILD__ARCH
+'''
+
 	def do_check_and_set_result(self, sched_ctx):
 		for x in BuildCheckTask.do_check_and_set_result(self, sched_ctx): yield x
 		r, out = self.results
-		if not r: self.results = False, None
-		else: self.results = True, out.split('\n')[-2][1:]
+		if not r: self.results = False, None, None, None
+		else:
+			out = out.split('\n')
+			self.results = True, out[-4][1:-1], out[-3][1:-1], out[-2][1:-1]
 	
 	@property
 	def result(self): return self.results[0]
 	
 	@property
 	def format(self): return self.results[1]
-	
+
+	@property
+	def os(self): return self.results[2]
+
+	@property
+	def arch(self): return self.results[3]
+
 	@property
 	def result_display(self):
 		if self.result: return self.format, ok_color
 		else: return 'unkown (neither of elf, mac-o, nor pe)', failed_color
+
+def unversioned_sys_platform_to_binary_format(unversioned_sys_platform):
+	"infers the binary format from the unversioned_sys_platform name."
+
+	if unversioned_sys_platform in ('linux', 'freebsd', 'netbsd', 'openbsd', 'sunos'): return 'elf'
+	elif unversioned_sys_platform == 'darwin': return 'mac-o'
+	elif unversioned_sys_platform in ('win', 'cygwin', 'uwin', 'msys'): return 'pe'
+	# TODO we assume all other operating systems are elf, which is not true.
+	# we may set this to 'unknown' and have ccroot and other tools handle the case "gracefully" (whatever that means).
+	return 'elf'
+
+def unversioned_sys_platform():
+	"""returns an unversioned name from sys.platform.
+	sys.plaform is not very well defined and depends directly on the python source tree.
+	The version appended to the names is unreliable as it's taken from the build environment at the time python was built,
+	i.e., it's possible to get freebsd7 on a freebsd8 system.
+	So we remove the version from the name (except for the stupid name os2).
+	Some possible values of sys.platform are, amongst others:
+		aix3 aix4 atheos beos5 darwin freebsd2 freebsd3 freebsd4 freebsd5 freebsd6 freebsd7
+		generic irix5 irix6 linux2 mac netbsd1 next3 os2emx riscos sunos5 unixware7
+	Investigating the python source tree may reveal more values.
+	"""
+	s = sys.platform
+	if s == 'java':
+		# The real OS is hidden under the JVM.
+		from java.lang import System
+		s = System.getProperty('os.name')
+		# see http://lopica.sourceforge.net/os.html for a list of possible values
+		if s == 'Mac OS X': return 'darwin'
+		elif s.startswith('Windows '): return 'win'
+		elif s == 'OS/2': return 'os2'
+		elif s == 'HP-UX': return 'hpux'
+		elif s in ('SunOS', 'Solaris'): return 'sunos'
+		else: s = s.lower()
+	if s.endswith('os2') and s != 'sunos2': return s
+	return re.split('\d+$', s)[0]
 
 class MSWindowsCheckTask(BuildCheckTask):
 	@staticmethod
