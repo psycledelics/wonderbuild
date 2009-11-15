aae7ad4d2388aaf915b1b6c540ab364a12df9bfd
Johan Boule <johan.boule@retropaganda.info>
2009-11-15 11:25:16 +0000

    pkg-config checks module versions
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@9043 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild_script_common.py
+++ build-systems/wonderbuild_script_common.py
@@ -46,7 +46,7 @@ class Wonderbuild(ScriptTask):
 			cfg.cxx_flags += ['-EHa', '-MD'] # basic compilation flags required
 
 		check_cfg = cfg.clone()
-		glibmm = PkgConfigCheckTask.shared(check_cfg, ['glibmm-2.4 >= 2.4', 'gmodule-2.0 >= 2.0', 'gthread-2.0 >= 2.0'])
+		glibmm = PkgConfigCheckTask.shared(check_cfg, ['glibmm-2.4', 'gmodule-2.0', 'gthread-2.0'])
 		std_math = StdMathCheckTask.shared(check_cfg)
 		dlfcn = DlfcnCheckTask.shared(check_cfg)
 		pthread = PThreadCheckTask.shared(check_cfg)
