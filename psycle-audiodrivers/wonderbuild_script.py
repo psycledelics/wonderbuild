fae5823576a60aa77c6890f612f3d9aa850da709
Johan Boule <johan.boule@retropaganda.info>
2010-01-30 14:30:23 +0000

    build each driver separately. old all-in-one driver still there for compat
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@9500 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- psycle-audiodrivers/wonderbuild_script.py
+++ psycle-audiodrivers/wonderbuild_script.py
@@ -45,12 +45,96 @@ class Wonderbuild(ScriptTask):
 		from wonderbuild.install import InstallTask
 
 		check_cfg = cfg.clone()
-		alsa = PkgConfigCheckTask.shared(check_cfg, ['alsa >= 1.0'])
+		gstreamer = PkgConfigCheckTask.shared(check_cfg, ['gstreamer-0.10'])
 		jack = PkgConfigCheckTask.shared(check_cfg, ['jack >= 0.101.1'])
+		alsa = PkgConfigCheckTask.shared(check_cfg, ['alsa >= 1.0'])
 		esound = PkgConfigCheckTask.shared(check_cfg, ['esound'])
-		gstreamer = PkgConfigCheckTask.shared(check_cfg, ['gstreamer-0.10 >= 0.10'])
-		winmm = WinMMCheckTask.shared(check_cfg)
 		dsound = DSoundCheckTask.shared(check_cfg)
+		winmm = WinMMCheckTask.shared(check_cfg)
+
+		for x in sched_ctx.parallel_wait(
+			gstreamer, jack, alsa, esound, dsound, winmm
+		): yield x
+
+		class UniformMod(ModTask):
+			def __init__(self, name, path, deps=None, kind=ModTask.Kinds.LOADABLE):
+				ModTask.__init__(self, name, kind, cfg, (name, 'default'))
+				self.path = path
+				if deps is not None: self.public_deps += deps
+
+			def __call__(self, sched_ctx):
+				if self.kind == ModTask.Kinds.PROG: self.private_deps = [pch.prog_task]
+				else: self.private_deps = [pch.lib_task]
+				self.public_deps += [helpers]
+				req = self.all_deps
+				for x in sched_ctx.parallel_wait(*req): yield x
+				self.result = min(bool(r) for r in req)
+				self.cxx_phase = self.__class__.InstallHeaders(self)
+				for x in ModTask.__call__(self, sched_ctx): yield x
+		
+			def do_mod_phase(self):
+				self.cfg.include_paths.appendleft(src_dir)
+				self.cfg.defines['UNIVERSALIS__META__MODULE__NAME'] = '"' + self.name +'"'
+				self.cfg.defines['UNIVERSALIS__META__MODULE__VERSION'] = 0
+				if self.path.exists:
+					for s in self.path.find_iter(in_pats = ('*.cpp',), prune_pats = ('todo',)): self.sources.append(s)
+				else: self.sources.append(self.path.parent / (self.path.name + '.cpp'))
+
+			def apply_cxx_to(self, cfg):
+				if not self.cxx_phase.dest_dir in cfg.include_paths: cfg.include_paths.append(self.cxx_phase.dest_dir)
+				ModTask.apply_cxx_to(self, cfg)
+
+			class InstallHeaders(InstallTask):
+				def __init__(self, outer):
+					InstallTask.__init__(self, outer.project, outer.name + '-headers')
+					self.outer = outer
+				
+				@property
+				def trim_prefix(self): return src_dir
+
+				@property
+				def dest_dir(self): return self.fhs.include
+
+				@property
+				def sources(self):
+					try: return self._sources
+					except AttributeError:
+						self._sources = []
+						if self.outer.path.exists:
+							for s in self.outer.path.find_iter(
+								in_pats = ('*.hpp',), ex_pats = ('*.private.hpp',), prune_pats = ('todo',)): self._sources.append(s)
+						f = self.outer.path.parent / (self.outer.path.name + '.hpp')
+						if f.exists: self._sources.append(f)
+						return self._sources
+		
+		class AudioDriverMod(UniformMod):
+			def __init__(self, name, path, deps=None, define_name=None):
+				UniformMod.__init__(self,
+					'psycle-audiodriver-' + name,
+					src_dir / 'psycle' / 'audiodrivers' / path,
+					deps
+				)
+				self.define_name = define_name is not None and define_name or name.replace('-', '_').upper()
+			
+			def apply_defines_to(self, cfg):
+				cfg.defines['PSYCLE__' + self.define_name + '_AVAILABLE'] = None
+		
+			def do_mod_phase(self):
+				self.apply_defines_to(self.cfg)
+				UniformMod.do_mod_phase(self)
+				
+			def apply_cxx_to(self, cfg): self.apply_defines_to(cfg)
+
+		if gstreamer: gst_driver = AudioDriverMod('gstreamer', 'gstreamerout', deps=(gstreamer,))
+		if alsa: alsa_driver = AudioDriverMod('alsa', 'alsaout', deps=(alsa,))
+		if esound: esound_driver = AudioDriverMod('esound', 'esoundout', deps=(esound,))
+		if dsound: dsound_driver = AudioDriverMod('microsoft-direct-sound', 'microsoftdirectsoundout', deps=(dsound,))
+		file_driver = AudioDriverMod('wave-file', 'wavefileout')
+		if False: # these drivers need testing
+			if jack: jack_driver = AudioDriverMod('jack', 'jackout', deps=(jack,))
+			if winmm: winmm_driver = AudioDriverMod('microsoft-mme', 'microsoftmmewaveout', deps=(winmm,))
+			# netaudio
+			# asio
 
 		class AudioDriversMod(ModTask):
 			def __init__(self):
