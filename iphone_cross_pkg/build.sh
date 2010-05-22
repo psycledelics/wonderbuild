e4d8126661a94fe316d19e225cb8d94aec4b8378
Johan Boule <johan.boule@retropaganda.info>
2010-05-22 22:35:11 +0000

    test on 32-bit os
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@9730 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- iphone_cross_pkg/build.sh
+++ iphone_cross_pkg/build.sh
@@ -8,13 +8,16 @@ set -x &&
 
 cd $(dirname $0) &&
 
+XPREFIX=/usr/local &&
+#USE_XPREFIX="--prefix=$XPREFIX" &&
+
 # llvm
 if ! test -d llvm-svn
 then
 	svn checkout -r42498 http://llvm.org/svn/llvm-project/llvm/trunk llvm-svn
 fi &&
 cd llvm-svn &&
-CC=gcc-4.1 CXX=g++-4.1 ./configure --enable-optimized &&
+CC=gcc-4.1 CXX=g++-4.1 ./configure $USE_XPREFIX --enable-optimized &&
 make ENABLE_OPTIMIZED=1 &&
 make install &&
 export LLVMOBJDIR=$(pwd) &&
@@ -38,46 +41,65 @@ export HEAVENLY=$(pwd)/heavenly &&
 # iphone-dev
 if ! test -d iphone-dev
 then
-	svn checkout http://iphone-dev.googlecode.com/svn/trunk iphone-dev
+	svn checkout http://iphone-dev.googlecode.com/svn/trunk iphone-dev &&
+	cd iphone-dev &&
+	cd include &&
+	svn switch http://iphone-dev.googlecode.com/svn/branches/include-1.2-sdk &&
+	cd .. &&
+	cd odcctools &&
+	svn switch http://iphone-dev.googlecode.com/svn/branches/odcctools-9.2-ld &&
+	cd ../..
 fi &&
 cd iphone-dev &&
 
 # ???
-if ! test -d /usr/local/arm-apple-darwin
+if ! test -d $XPREFIX/arm-apple-darwin
 then
-	mkdir /usr/local/arm-apple-darwin
+	mkdir $XPREFIX/arm-apple-darwin
 fi &&
 
 # iphone-dev odcctools
 mkdir -p build/odcctools &&
 cd build/odcctools &&
-export INCPRIVEXT="-isysroot $SDK" &&
-CC=gcc-4.1 CXX=g++-4.1 ../../odcctools/configure --target=arm-apple-darwin --disable-ld64
+../../odcctools/configure $USE_XPREFIX --target=arm-apple-darwin --disable-ld64
 make &&
 make install &&
 cd ../.. &&
 
 # iphone headers installation
 cd include &&
-./configure --with-macosx-sdk=$SDK &&
+./configure $USE_XPREFIX --with-macosx-sdk=$SDK &&
 bash install-headers.sh &&
 cd .. &&
 
 # iphone-dev csu
 mkdir -p build/csu &&
 cd build/csu &&
-../../csu/configure --host=arm-apple-darwin
+../../csu/configure $USE_XPREFIX --host=arm-apple-darwin
 make install &&
 cd ../.. &&
+
+# iphone-dev issue #145
+if ! test -f lib1funcs.asm.diff
+then
+	wget 'http://iphone-dev.googlecode.com/issues/attachment?aid=-4811806783580725987&name=lib1funcs.asm.diff&token=9191c5c2ddd1a1cad1f8ddec6b288cb2' \
+		--output-document=lib1funcs.asm.diff &&
+	cd llvm-gcc-4.0-iphone/gcc/config/arm &&
+	patch -p0 < ../../../../lib1funcs.asm.diff &&
+	cd ../../../..
+fi &&
 
 # iphone-dev llvm-gcc
 mkdir -p build/llvm-gcc-4.0-iphone &&
 cd build/llvm-gcc-4.0-iphone &&
-CC=gcc-4.1 CXX=g++-4.1 ../../llvm-gcc-4.0-iphone/configure --enable-llvm=$LLVMOBJDIR --with-heavenly=$HEAVENLY \
+../../llvm-gcc-4.0-iphone/configure $USE_XPREFIX --enable-llvm=$LLVMOBJDIR --with-heavenly=$HEAVENLY \
 	--enable-languages=c,c++,objc,obj-c++ --target=arm-apple-darwin --enable-sjlj-exceptions --enable-wchar_t=no \
-	--with-as=/usr/local/bin/arm-apple-darwin-as \
-	--with-ld=/usr/local/bin/arm-apple-darwin-ld &&
-make LLVM_VERSION_INFO=2.0-svn-iphone-dev-0.3-svn &&
+	--with-as=$XPREFIX/bin/arm-apple-darwin-as \
+	--with-ld=$XPREFIX/bin/arm-apple-darwin-ld &&
+#ln -sf gcc/darwin/4.0/stdint.h $XPREFIX/arm-apple-darwin/include/ &&
+####ln -s libSystem.B.dylib libSystem.dylib
+####ln -s libSystem.dylib libc.dylib
+make LLVM_VERSION_INFO=2.0-svn-iphone-dev-0.3-svn && #NM=$XPREFIX/bin/arm-apple-darwin-nm &&
 make install &&
 cd ../.. &&
 
