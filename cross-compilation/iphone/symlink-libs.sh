4bf964dbb09defc2c8870c36ea9fb3a3b71de6ad
Johan Boule <johan.boule@retropaganda.info>
2010-05-23 14:46:34 +0000

    some 3d test app
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@9736 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ cross-compilation/iphone/symlink-libs.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -x &&
+
+cd $(dirname $0) &&
+
+for lib in System objc
+do
+	ln -s \
+		../../../../sdks/iPhoneOS3.2.sdk/usr/lib/lib$lib.dylib \
+		iphonedevonlinux/toolchain/sys/usr/lib/ || :
+done &&
+
+for lib in Foundation CoreFoundation CoreGraphics UIKit
+do
+	ln -s \
+		../../../../../../sdks/iPhoneOS3.2.sdk/System/Library/Frameworks/$lib.framework/$lib \
+		iphonedevonlinux/toolchain/sys/System/Library/Frameworks/$lib.framework/ || :
+done
