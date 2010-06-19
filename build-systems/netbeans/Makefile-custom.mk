49232ce90f066647f0975a11a37c7f57ea7e0994
Johan Boule <johan.boule@retropaganda.info>
2010-06-19 20:45:12 +0000

    print abs paths to allow hyperlinks in build log
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@9757 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/netbeans/Makefile-custom.mk
+++ build-systems/netbeans/Makefile-custom.mk
@@ -1,7 +1,7 @@
 dir = ../wonderbuild
 
 all:
-	$(dir)/wonderbuild_script.py --cxx-flags='-g3 -gdwarf-2'
+	$(dir)/wonderbuild_script.py --source-abs-paths=yes --cxx-flags='-g3 -gdwarf-2'
 
 clean:
 	rm -Rfv $(dir)/++wonderbuild
