b666958a1af7660176b31ebef5e5cb90df964301
Johan Boule <johan.boule@retropaganda.info>
2010-06-27 22:29:09 +0000

    misc
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@9781 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/wonderbuild_script.py
+++ build-systems/wonderbuild/wonderbuild_script.py
@@ -23,7 +23,7 @@ else:
 
 	class Wonderbuild(ScriptTask):
 		def __call__(self, sched_ctx):
-			tasks = (
+			tasks = [
 				ScriptLoaderTask.shared(self.project, self.src_dir.parent.parent / dir) \
 				for dir in (
 					'universalis',
@@ -33,7 +33,7 @@ else:
 					'psycle-player',
 					'psycle-plugins'
 				)
-			)
+			]
 			for x in sched_ctx.parallel_wait(*tasks): yield x
 			for t in tasks: self.default_tasks += t.script_task.default_tasks
 
