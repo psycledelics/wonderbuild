d759787d983136bab394bbae2973f6cf5c7b3566
Johan Boule <johan.boule@retropaganda.info>
2010-07-04 02:02:09 +0000

    frameworks
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@9813 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- cross-compilation/iphone/symlink-libs.sh
+++ cross-compilation/iphone/symlink-libs.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2010-2010 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+# iphonedevonlinux/toolchain.sh forgets to copy some files from the iphone sdk to the sysroot tree,
+# so this script adds the missing files afterwards.
 
 set -x &&
 
