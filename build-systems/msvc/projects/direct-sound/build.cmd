07f84a97cf76e14cb9fbc934fb43b0c28ac086a2
jaz001 <jaz001>
2011-03-06 17:01:54 +0000

    making dsound9 dir same as branch.
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@9984 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/msvc/projects/direct-sound/build.cmd
+++ build-systems/msvc/projects/direct-sound/build.cmd
@@ -11,14 +11,18 @@ if not exist %output%\direct-sound-stamp (
 	if not exist %output%\debug\lib (
 		mkdir %output%\debug\lib || exit /b 1
 	)
-	echo xcopy /f %pkgdir%\%libdir%\dsound.lib %output%\debug\lib\
-	xcopy /f %pkgdir%\%libdir%\dsound.lib %output%\debug\lib\ || exit /b 1
+	echo xcopy /Y /f %pkgdir%\%libdir%\dsound.lib %output%\debug\lib\
+	xcopy /Y /f %pkgdir%\%libdir%\dsound.lib %output%\debug\lib\ || exit /b 1
+	echo xcopy /Y /f %pkgdir%\%libdir%\dxguid.lib %output%\debug\lib\
+	xcopy /Y /f %pkgdir%\%libdir%\dxguid.lib %output%\debug\lib\ || exit /b 1
 
 	if not exist %output%\release\lib (
 		mkdir %output%\release\lib || exit /b 1
 	)
-	echo xcopy /f %pkgdir%\%libdir%\dsound.lib %output%\release\lib\
-	xcopy /f %pkgdir%\%libdir%\dsound.lib %output%\release\lib\ || exit /b 1
+	echo xcopy /Y /f %pkgdir%\%libdir%\dsound.lib %output%\release\lib\
+	xcopy /Y /f %pkgdir%\%libdir%\dsound.lib %output%\release\lib\ || exit /b 1
+	echo xcopy /Y /f %pkgdir%\%libdir%\dxguid.lib %output%\release\lib\
+	xcopy /Y /f %pkgdir%\%libdir%\dxguid.lib %output%\release\lib\ || exit /b 1
 
 	echo direct_sound copied > %output%\direct-sound-stamp || exit /b 1
 )
