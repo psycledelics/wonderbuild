7e8da9c63932f8d2c5fac70e5d4706375faec308
Johan Boule <johan.boule@retropaganda.info>
2011-06-12 14:39:25 +0000

    netbeans and wonderbuild
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@10057 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/netbeans/Makefile-custom.mk
+++ build-systems/netbeans/Makefile-custom.mk
@@ -2,7 +2,7 @@ dir = ../wonderbuild
 
 all:
 	$(dir)/wonderbuild_script.py \
-		--jobs=1 --sync-log --source-abs-paths=yes \
+		--jobs=1 --sync-log --input-abs-paths=yes \
 		--cxx-flags='-g3 -gdwarf-2'
 
 clean:
