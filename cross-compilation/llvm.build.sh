57d8a9f941ea4f22782b5e551e4c14add33610c0
Johan Boule <johan.boule@retropaganda.info>
2011-08-15 23:27:09 +0000

    svn:externals
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@10237 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- cross-compilation/llvm.build.sh
+++ cross-compilation/llvm.build.sh
@@ -2,5 +2,10 @@
 set -x &&
 cd $(dirname $0)/llvm &&
 svn up &&
-./configure --prefix=/tmp/install --enable-optimized --disable-assertions --enable-threads "$@"
+
+mkdir -p ++build &&
+cd ++build &&
+
+../configure --prefix=/tmp/install --enable-optimized --disable-assertions --enable-threads "$@"
+
 make -j8 install
