6d70b84a5bffb22857155e55d9bec7ca23425bab
Johan Boule <johan.boule@retropaganda.info>
2011-09-11 08:01:27 +0000

    prefix
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@10251 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- cross-compilation/llvm.build.sh
+++ cross-compilation/llvm.build.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
+
 set -x &&
+
 cd $(dirname $0)/llvm &&
+
 svn up &&
 
 mkdir -p ++build &&
 cd ++build &&
 
-../configure --prefix=/tmp/install --enable-optimized --disable-assertions --enable-threads "$@"
+../configure --prefix=$(cd ../++install && pwd) --enable-optimized --disable-assertions --enable-threads "$@"
 
 make -j8 install
