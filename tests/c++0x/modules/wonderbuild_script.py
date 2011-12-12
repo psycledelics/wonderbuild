3a678b1890245b710ecc3e472de536d91939485e
Johan Boule <johan.boule@retropaganda.info>
2011-12-12 02:55:37 +0000

    module impl test
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@10361 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ tests/c++0x/modules/wonderbuild_script.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2009-2011 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+if __name__ == '__main__':
+	import sys, os
+	dir = os.path.dirname(__file__)
+	sys.argv.append('--src-dir=' + dir)
+	try: from wonderbuild.main import main
+	except ImportError:
+		dir = os.path.abspath(os.path.join(dir, os.pardir, os.pardir, 'build-systems', 'wonderbuild'))
+		if dir not in sys.path: sys.path.append(dir)
+		try: from wonderbuild.main import main
+		except ImportError:
+			print >> sys.stderr, 'could not find wonderbuild'
+			sys.exit(1)
+		else: main()
+	else: main()
+
+from wonderbuild.script import ScriptTask
+
+class Wonderbuild(ScriptTask):
+	def __call__(self, sched_ctx):
+		project = self.project
+		top_src_dir = self.src_dir
+		src_dir = self.src_dir
+		default_tasks = self.default_tasks
+
+		from wonderbuild.cxx_tool_chain import UserBuildCfgTask
+		from wonderbuild.cxx_tool_chain import ModTask
+		from wonderbuild.install import InstallTask
+
+		cfg = UserBuildCfgTask.shared(project)
+		for x in sched_ctx.parallel_wait(cfg): yield x
+		cfg = cfg.clone()
+
+		class UniformMod(ModTask):
+			def __init__(self, name, path, deps=None, kind=ModTask.Kinds.LOADABLE):
+				ModTask.__init__(self, name, kind, cfg)
+				self.path = path
+				if deps is not None: self.public_deps += deps
+				if kind in (ModTask.Kinds.PROG, ModTask.Kinds.LOADABLE): default_tasks.append(self.mod_phase)
+				self.cxx_phase = self.__class__.InstallHeaders(self)
+
+			def __call__(self, sched_ctx):
+				for x in ModTask.__call__(self, sched_ctx): yield x
+			
+			def do_mod_phase(self):
+				self.cfg.include_paths.appendleft(src_dir)
+				if self.path.exists:
+					for s in self.path.find_iter(in_pats = ('*.cpp',)): self.sources.append(s)
+				else: self.sources.append(self.path.parent / (self.path.name + '.cpp'))
+
+			def apply_cxx_to(self, cfg):
+				if not self.cxx_phase.dest_dir in cfg.include_paths: cfg.include_paths.append(self.cxx_phase.dest_dir)
+				ModTask.apply_cxx_to(self, cfg)
+
+			class InstallHeaders(InstallTask):
+				def __init__(self, outer):
+					InstallTask.__init__(self, outer.base_cfg.project, outer.name + '-headers')
+					self.outer = outer
+					
+				@property
+				def trim_prefix(self): return src_dir
+
+				@property
+				def dest_dir(self): return self.fhs.include
+
+				@property
+				def sources(self):
+					try: return self._sources
+					except AttributeError:
+						self._sources = []
+						if self.outer.path.exists:
+							for s in self.outer.path.find_iter(
+								in_pats = ('*.hpp',), ex_pats = ('*.private.hpp',)): self._sources.append(s)
+						f = self.outer.path.parent / (self.outer.path.name + '.hpp')
+						if f.exists: self._sources.append(f)
+						return self._sources
+
+		ops = UniformMod('ops', src_dir / 'ops', kind=ModTask.Kinds.LIB)
+		crapculator = UniformMod('crapculator', src_dir / 'crapculator', kind=ModTask.Kinds.LIB, deps=(ops,))
+		main = UniformMod('main', src_dir / 'main', kind=ModTask.Kinds.PROG, deps=(crapculator,))
+
