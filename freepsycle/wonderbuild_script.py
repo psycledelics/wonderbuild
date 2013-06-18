03d7c44f6c2d927d3a7ee70d6a75d76caf65848b
Johan Boule <johan.boule@retropaganda.info>
2013-06-18 19:36:59 +0000

    on debian stable, there is no python2 alias
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@10528 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- freepsycle/wonderbuild_script.py
+++ freepsycle/wonderbuild_script.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python
 # This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
 # copyright 2009-2011 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
 
