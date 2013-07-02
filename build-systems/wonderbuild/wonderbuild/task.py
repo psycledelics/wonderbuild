4d8d80d099ede48172abc526573fe203ebb3c4e2
Johan Boule <johan.boule@retropaganda.info>
2013-07-02 22:27:12 +0000

    note about cached decorator
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@10541 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/wonderbuild/task.py
+++ build-systems/wonderbuild/wonderbuild/task.py
@@ -4,6 +4,35 @@
 
 from logger import is_debug, debug, out, cols, colored, multicolumn_format
 
+if False:
+	# TODO @task decorator : allow tasks directly on functions
+	# TODO This would also speed up attributes in filesystem.py
+	class cache(object):
+		'''Computes attribute value and caches it in the instance.
+		Python Cookbook (Denis Otkidach) http://stackoverflow.com/users/168352/denis-otkidach
+		This decorator allows you to create a property which can be computed once and
+		accessed many times. Sort of like memoization.
+
+		'''
+		def __init__(self, method, name=None):
+			# record the unbound-method and the name
+			self.method = method
+			self.name = name or method.__name__
+			self.__doc__ = method.__doc__
+		def __get__(self, inst, cls):
+			# self: <__main__.cache object at 0xb781340c>
+			# inst: <__main__.Foo object at 0xb781348c>
+			# cls: <class '__main__.Foo'>       
+			if inst is None:
+				# instance attribute accessed on class, return self
+				# You get here if you write `Foo.bar`
+				return self
+			# compute, cache and return the instance's attribute value
+			result = self.method(inst)
+			# setattr redefines the instance's attribute so this doesn't get called again
+			setattr(inst, self.name, result)
+			return result
+
 class Task(object):
 
 	if False: # This is now done lazily directly in the Scheduler.
