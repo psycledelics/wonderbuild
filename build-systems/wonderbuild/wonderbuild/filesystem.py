41d6d9ff9ed497d45d5aeaed097f9869ce415002
Johan Boule <johan.boule@retropaganda.info>
2013-07-02 18:56:41 +0000

    don't use ctime because it is also updated when making a hard link
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@10540 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/wonderbuild/wonderbuild/filesystem.py
+++ build-systems/wonderbuild/wonderbuild/filesystem.py
@@ -100,7 +100,9 @@ class Node(object):
 				if e.errno != errno.ENOENT: raise
 				# broken symlink
 		self._is_dir = stat.S_ISDIR(st.st_mode)
-		self._time = st.st_ctime # note: unlike mtime, ctime is updated when a change in perm or ownership occurs.
+		# note: Unlike mtime, ctime is updated when a change in perm or ownership occurs.
+		# note: We don't use ctime because it is also updated when making a hard link.
+		self._time = st.st_mtime
 
 	@property
 	def lock(self):
