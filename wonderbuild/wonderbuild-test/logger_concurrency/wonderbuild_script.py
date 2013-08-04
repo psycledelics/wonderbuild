1451c3dffe68ea54d1e6db59495f5ddfd3b83e71
Johan Boule <johan.boule@retropaganda.info>
2013-08-04 16:18:56 +0000

    task decorator, logger concurrency
    git-svn-id: http://svn.code.sf.net/p/psycle/code/branches/bohan@10547 f55022e6-6845-49fe-a7ed-519222bdd1fd

---
+++ wonderbuild/wonderbuild-test/logger_concurrency/wonderbuild_script.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2013-2013 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+
+##############################################################################
+#
+# this script tests the logger's concurrency
+#
+##############################################################################
+
+
+if __name__ == '__main__':
+	import sys, os
+	dir = os.path.dirname(__file__)
+	sys.argv.append('--src-dir=' + dir)
+	try: from wonderbuild.main import main
+	except ImportError:
+		dir = os.path.abspath(os.path.join(dir, os.pardir, os.pardir))
+		if dir not in sys.path: sys.path.append(dir)
+		try: from wonderbuild.main import main
+		except ImportError:
+			print >> sys.stderr, 'could not find wonderbuild'
+			sys.exit(1)
+		else: main()
+	else: main()
+
+else:
+	from wonderbuild.script import ScriptTask
+	from wonderbuild.task import Task
+	from wonderbuild.subprocess_wrapper import exec_subprocess
+
+	class Wonderbuild(ScriptTask):
+		class Foo(Task):
+			def __call__(self, sched_ctx):
+				if False: yield
+				sched_ctx.lock.release()
+				try: exec_subprocess(['sh', '-c', "for i in 1 2 3; do echo -n $i f; sleep 1; echo -n o; sleep 1; echo o; done"])
+				finally: sched_ctx.lock.acquire()
+		class Bar(Task):
+			def __call__(self, sched_ctx):
+				if False: yield
+				sched_ctx.lock.release()
+				try: exec_subprocess(['sh', '-c', "for i in 1 2 3; do echo -n $i b; sleep 1; echo -n a; sleep 1; echo r; done"])
+				finally: sched_ctx.lock.acquire()
+		def __call__(self, sched_ctx):
+			tasks = Wonderbuild.Foo(), Wonderbuild.Bar()
+			for x in sched_ctx.parallel_wait(*tasks): yield x
---
+++ wonderbuild/wonderbuild-test/task_decorator/wonderbuild_script.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+# This source is free software ; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ; either version 2, or (at your option) any later version.
+# copyright 2013-2013 members of the psycle project http://psycle.sourceforge.net ; johan boule <bohan@jabber.org>
+
+
+##############################################################################
+#
+# this script tests the @task decorator
+#
+##############################################################################
+
+
+if __name__ == '__main__':
+	import sys, os
+	dir = os.path.dirname(__file__)
+	sys.argv.append('--src-dir=' + dir)
+	try: from wonderbuild.main import main
+	except ImportError:
+		dir = os.path.abspath(os.path.join(dir, os.pardir, os.pardir))
+		if dir not in sys.path: sys.path.append(dir)
+		try: from wonderbuild.main import main
+		except ImportError:
+			print >> sys.stderr, 'could not find wonderbuild'
+			sys.exit(1)
+		else: main()
+	else: main()
+
+else:
+	from wonderbuild.script import ScriptTask
+	from wonderbuild.task import task
+
+	class Wonderbuild(ScriptTask):
+		def __call__(self, sched_ctx):
+				for x in sched_ctx.parallel_wait(self.task1, self.task2): yield x
+
+		@task
+		def task1(self, sched_ctx):
+				print 'begin 1'
+				for x in sched_ctx.parallel_wait(self.task3): yield x
+				print 'end 1'
+
+		@task
+		def task2(self, sched_ctx):
+				print 'begin 2'
+				for x in sched_ctx.parallel_wait(self.task3): yield x
+				print 'end 2'
+
+		@task
+		def task3(self, sched_ctx):
+				print 'begin 3'
+				print 'end 3'
