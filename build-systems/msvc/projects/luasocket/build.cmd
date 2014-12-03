4424c408fd471f3a63eee7a36f94e8341efc613b
jaz001 <jaz001>
2014-12-03 18:46:27 +0000

    trying to fix vs 2010 build.
    git-svn-id: http://svn.code.sf.net/p/psycle/code/trunk@10665 f55022e6-6845-49fe-a7ed-519222bdd1fd

--- build-systems/msvc/projects/luasocket/build.cmd
+++ build-systems/msvc/projects/luasocket/build.cmd
@@ -6,7 +6,7 @@ cd %~p0
 set pkgdir=..\..\..\..\external-packages\luasocket-%1
 set srcdirname=luasocket-%1
 
-if not exist %pkgdir%\src (
+if not exist %pkgdir%\luasocket (
 	pushd %pkgdir% || exit /b 1
 	..\7za\7z.exe x -y %srcdirname%.tar.bz2 || exit /b 1
 	..\7za\7z.exe x -y %srcdirname%.tar || exit /b 1
